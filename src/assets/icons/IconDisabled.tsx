d99d7ccd1c4ef27c9f0ea32df9aa1f8e730d21b1
Reinny Almonte <reinny.almonte@pm.me>
2022-10-18 16:46:41 -0400

    RIA-3: Adding IconArrowDown + IconSearch + IconDisabled

---
+++ src/assets/icons/IconDisabled.tsx
@@ -0,0 +1,27 @@
+import { IconProps } from './props';
+
+export const IconDisabled = ({ className }: IconProps) => (
+  <svg
+    className={className}
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="16.5"
+      cy="16.5"
+      r="5.75"
+      transform="rotate(45 16.5 16.5)"
+      strokeWidth="1.5"
+    />
+    <line
+      x1="21.2729"
+      y1="12.0806"
+      x2="12.7877"
+      y2="20.5658"
+      strokeWidth="1.5"
+    />
+  </svg>
+);
---
+++ src/assets/icons/IconSearch.tsx
@@ -0,0 +1,26 @@
+import { IconProps } from './props';
+
+export const IconSearch = ({ className }: IconProps) => (
+  <svg
+    className={className}
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="14.5"
+      cy="14.5"
+      r="4.75"
+      strokeWidth={1.5}
+    />
+    <line
+      x1="18.4857"
+      y1="17.4251"
+      x2="22.5302"
+      y2="21.4696"
+      strokeWidth={1.5}
+    />
+  </svg>
+);
