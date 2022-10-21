225f45a293ef26d62a4b033e81b8524fe475457e
Reinny Almonte <reinny.almonte@pm.me>
2022-10-21 16:50:40 -0400

    RIA-21: Defining IconArrowDown + IconArrowUp icons

---
+++ src/assets/icons/IconArrowUp.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from './props';
+
+export const IconArrowUp = ({ className }: IconProps) => (
+  <svg
+    className={className}
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M11 19L16 14L21 19"
+      stroke="black"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
