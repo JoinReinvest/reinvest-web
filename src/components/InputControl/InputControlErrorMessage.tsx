deae20b38cbffcb59fe3b2d69011af8e9cb92aaf
Reinny Almonte <reinny.almonte@pm.me>
2022-11-14 19:42:22 -0400

    chore: deprecating InputControl and TextInput components

--- src/components/InputControl/InputControlErrorMessage.tsx
+++
@@ -1,7 +0,0 @@
-interface InputControlErrorMessageProps {
-  message?: string;
-}
-
-export const InputControlErrorMessage = ({ message }: InputControlErrorMessageProps) => (
-  <div className="paragraph-small absolute left-0 mt-8 top-full text-secondary-error">{message}</div>
-);
