b8f777f00edc525681728803367f9ecbef0ca7cb
Reinny Almonte <reinny.almonte@pm.me>
2022-11-21 18:41:33 -0400

    refactor: using @hookooekoo/ui-checkbox for Checkbox component

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,31 +1,14 @@
-import { Indicator, Root as CheckboxRoot } from '@radix-ui/react-checkbox';
+import { Checkbox as PrimitiveCheckbox, CheckboxProps as PrimitiveCheckboxProps } from '@hookooekoo/ui-checkbox';
 import { IconCheckmark } from 'assets/icons/IconCheckmark';
-import cx from 'classnames';
-import { Dispatch, SetStateAction } from 'react';
 
-export interface CheckboxProps {
-  isChecked: boolean;
-  onChange: Dispatch<SetStateAction<boolean>>;
-  isDisabled?: boolean;
-}
+type Props = Omit<PrimitiveCheckboxProps, 'children'>;
 
-export const Checkbox = ({ isChecked = false, onChange, isDisabled = false }: CheckboxProps) => {
-  const containerStyles = cx(
-    'h-24 w-24 p-2 flex items-center rounded-full',
-    'radix-state-checked:bg-secondary-5',
-    'radix-state-unchecked:border radix-state-unchecked:border-secondary-5',
-  );
-
-  return (
-    <CheckboxRoot
-      disabled={isDisabled}
-      checked={isChecked}
-      onCheckedChange={value => onChange(!!value)}
-      className={containerStyles}
-    >
-      <Indicator>
-        <IconCheckmark className="max-w-full max-h-full" />
-      </Indicator>
-    </CheckboxRoot>
-  );
-};
+export const Checkbox = ({ isChecked = false, onChange, isDisabled = false }: Props) => (
+  <PrimitiveCheckbox
+    isDisabled={isDisabled}
+    isChecked={isChecked}
+    onChange={onChange}
+  >
+    <IconCheckmark className="max-w-full max-h-full" />
+  </PrimitiveCheckbox>
+);
