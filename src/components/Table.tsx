bf39b503f70eb50b6f967165e77f4edcf8e29b0f
Reinny Almonte <reinny.almonte@pm.me>
2022-11-22 10:39:59 -0400

    refactor: Table component uses @hookooekoo/ui-table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,54 +1,11 @@
-import { ColumnDef, createColumnHelper, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import { Table as PrimitiveTable } from '@hookooekoo/ui-table';
+import { ComponentProps } from 'react';
 
-interface TableProps<TData> {
-  columns: ColumnDef<TData, unknown>[];
-  data: TData[];
-}
-
-export function useTableColumnHelper<TData>() {
-  return createColumnHelper<TData>();
-}
-
-export function Table<TData>({ data, columns }: TableProps<TData>) {
-  const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel() });
-
+export function Table<TData>({ data, columns }: ComponentProps<typeof PrimitiveTable<TData>>) {
   return (
-    <table className="table-auto text-12 border border-secondary-5 select-none">
-      <thead className="text-secondary-3">
-        {table.getHeaderGroups().map(headerGroup => (
-          <tr
-            key={headerGroup.id}
-            className="border-b border-b-secondary-5"
-          >
-            {headerGroup.headers.map(header => (
-              <th
-                key={header.id}
-                className="pt-16 pb-10 px-30 font-normal"
-              >
-                {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-
-      <tbody className="text-green-deep">
-        {table.getRowModel().rows.map(row => (
-          <tr
-            key={row.id}
-            className="border-b border-b-secondary-5"
-          >
-            {row.getVisibleCells().map(cell => (
-              <td
-                key={cell.id}
-                className="py-10 px-30"
-              >
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <PrimitiveTable
+      data={data}
+      columns={columns}
+    />
   );
 }
