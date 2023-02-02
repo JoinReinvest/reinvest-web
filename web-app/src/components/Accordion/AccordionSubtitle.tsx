7f4d411e938be8d7a9617cb12bd449e899357531
Reinny Almonte <reinny@hookooekoo.co>
2023-02-02 16:51:29 -0400

    fix: breaking changes

--- web-app/src/components/Accordion/AccordionSubtitle.tsx
+++ web-app/src/components/Accordion/AccordionSubtitle.tsx
@@ -1,5 +1,13 @@
 import { AccordionProps } from './interfaces';
+import { Typography } from 'components/Typography';
 
 type Props = Pick<AccordionProps, 'subtitle'>;
 
-export const AccordionSubtitle = ({ subtitle }: Props) => <p className="text-12 text-secondary-3">{subtitle}</p>;
+export const AccordionSubtitle = ({ subtitle }: Props) => (
+  <Typography
+    variant="paragraph"
+    className="text-secondary-3"
+  >
+    {subtitle}
+  </Typography>
+);
