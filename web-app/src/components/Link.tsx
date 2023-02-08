85eefda6420b6f5fc930eac2ea65ae54def531c3
Reinny Almonte <reinny@hookooekoo.co>
2023-02-07 23:15:31 -0400

    feat: allowing to pass className to Link component

--- web-app/src/components/Link.tsx
+++ web-app/src/components/Link.tsx
@@ -1,11 +1,14 @@
 import { Link as PrimitiveLink } from '@hookooekoo/ui-link';
 import { ComponentProps } from 'react';
+import cx from 'classnames';
 
-type Props = Omit<ComponentProps<typeof PrimitiveLink>, 'className'>;
+type Props = ComponentProps<typeof PrimitiveLink>;
 
-export const Link = (props: Props) => (
+export const Link = ({ className, children, ...props }: Props) => (
   <PrimitiveLink
     {...props}
-    className="typo-link text-green-frost-01"
-  />
+    className={cx('typo-link text-green-frost-01', className)}
+  >
+    {children}
+  </PrimitiveLink>
 );
