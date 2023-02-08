c7eaa2e94257fdef7e09d1245d7f04e5f2e13390
Mateusz Solecki <solmat@gmail.com>
2023-02-08 21:36:39 +0100

    update

--- web-app/src/pages/onboarding/enter-date-birth.tsx
+++ web-app/src/pages/onboarding/enter-date-birth.tsx
@@ -1,43 +1,34 @@
-import { InputMasked, InputMaskedProps } from '@hookooekoo/ui-input-masked';
 import { BlackModal } from 'components/BlackModal';
 import { Link } from 'components/Link';
 import { Title } from 'components/Title';
 import { MainLayout } from 'layouts/MainLayout';
 import { NextPage } from 'next';
-import { useState } from 'react';
+
+import { BirthDateInput } from '../../components/BirthDateInput';
 
 const EnterDateBirthPage: NextPage = () => {
-  const [dateBirth, setDateBirth] = useState('');
-
-  const birthMask: InputMaskedProps['maskOptions'] = {
-    mask: '00/00/0000',
-  };
-
   return (
     <MainLayout>
       <BlackModal
         isOpen={true}
         onOpenChange={() => {
-          console.log(1); // eslint-disable-line
+          console.log(1) // eslint-disable-line
         }}
       >
-        <div className="flex flex-col gap-60">
-          <Title title="Enter your date of birth" />
-          <div>
-            <InputMasked
-              maskOptions={birthMask}
-              name="phone_number"
-              value={dateBirth}
-              onChange={updatedValue => setDateBirth(updatedValue)}
-              placeholder="Date of Birth"
-            />
-            <Link
-              href="/"
-              title="why required link"
-            >
-              Required. Why?
-            </Link>
-          </div>
+        <Title title="Enter your date of birth" />
+        <BirthDateInput
+          date={'00/00/0000'}
+          onChange={() => {
+          console.log(1) // eslint-disable-line
+          }}
+        />
+        <div className="mt-20 block">
+          <Link
+            href="/"
+            title="why required link"
+          >
+            Required. Why?
+          </Link>
         </div>
       </BlackModal>
     </MainLayout>
