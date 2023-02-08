2afcd20b615994d747e3c48c0aa2f857a745bff4
Reinny Almonte <reinny@hookooekoo.co>
2023-02-07 23:24:25 -0400

    refactor: enhancing TextInput component

--- web-app/src/components/TextInput/index.tsx
+++ web-app/src/components/TextInput/index.tsx
@@ -3,22 +3,24 @@ import { IconArrowDown } from 'assets/icons/IconArrowDown';
 import { IconDisabled } from 'assets/icons/IconDisabled';
 import { IconSearch } from 'assets/icons/IconSearch';
 import cx from 'classnames';
+import { useMemo } from 'react';
+import { FieldValues, useController } from 'react-hook-form';
 
 import { TextInputProps } from './interfaces';
 
-export const TextInput = ({
-  name,
-  placeholder,
-  required = false,
-  onChange,
-  onBlur,
-  onFocus,
-  value,
-  error,
-  disabled = false,
-  showArrowIcon = false,
-  showSearchIcon = false,
-}: TextInputProps) => {
+export const TextInput = <FormValues extends FieldValues>(props: TextInputProps<FormValues>) => {
+  const { field, fieldState } = useController<FormValues>(props);
+  const { name, placeholder, required = false, onBlur, onFocus, disabled = false, showArrowIcon = false, showSearchIcon = false, type = 'text' } = props;
+  const fieldValue = useMemo(() => field.value, [field]);
+
+  const onChange: TextInputProps<FieldValues>['onChange'] = event => {
+    if (props.type === 'number') {
+      return field.onChange(event.target.valueAsNumber);
+    }
+
+    return field.onChange(event.target.value);
+  };
+
   const iconLeftClass = cx({
     'absolute w-32 h-32 stroke-01': true,
     'stroke-gray-03': !!disabled,
@@ -31,17 +33,19 @@ export const TextInput = ({
 
   return (
     <PrimitiveTextInput
+      {...field}
       name={name}
-      value={value}
+      value={fieldValue}
       placeholder={placeholder}
       required={required}
       disabled={disabled}
       onChange={onChange}
       onBlur={onBlur}
       onFocus={onFocus}
-      error={error}
+      error={fieldState.error?.message}
       iconLeft={showSearchIcon && <IconSearch className={iconLeftClass} />}
       iconRight={showArrowIcon && !disabled ? <IconArrowDown className={iconRightClass} /> : disabled && <IconDisabled className={iconRightClass} />}
+      type={type}
     />
   );
 };
