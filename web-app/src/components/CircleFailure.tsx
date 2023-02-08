4ae60cd85d59582ac7dfa8bf8d060e46e696988d
Reinny Almonte <reinny@hookooekoo.co>
2023-02-08 09:51:38 -0400

    feat: defining CircleFailure component

---
+++ web-app/src/components/CircleFailure.tsx
@@ -0,0 +1,7 @@
+import { IconX } from 'assets/icons/IconX';
+
+export const CircleFailure = () => (
+  <div className="grid h-96 w-96 place-items-center rounded-full border border-white">
+    <IconX />
+  </div>
+);
