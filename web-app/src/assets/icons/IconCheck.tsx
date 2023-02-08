e31c4223d0f6ed0f43201b24978fb4eb047dfbf2
Reinny Almonte <reinny@hookooekoo.co>
2023-02-07 23:14:57 -0400

    feat: adding "check" + "success" icons

---
+++ web-app/src/assets/icons/IconCheck.tsx
@@ -0,0 +1,28 @@
+import { Icon } from '@hookooekoo/ui-icon';
+
+export const IconCheck = Icon(
+  <svg
+    width="25"
+    height="24"
+    viewBox="0 0 25 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_19_17599)">
+      <path
+        d="M7.64453 11.75L11.1659 15.25L18.2088 8.25"
+        strokeWidth="1.5"
+        strokeLinecap="square"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_19_17599">
+        <rect
+          width="24.1468"
+          height="24"
+          transform="translate(0.853516)"
+        />
+      </clipPath>
+    </defs>
+  </svg>,
+);
--- web-app/src/assets/icons/IconCheckmark.tsx
+++ web-app/src/assets/icons/IconCheckmark.tsx
@@ -28,3 +28,31 @@ export const IconCheckmark = Icon(
     </defs>
   </svg>,
 );
+
+<svg
+  width="25"
+  height="24"
+  viewBox="0 0 25 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <g clip-path="url(#clip0_19_17599)">
+    <path
+      d="M7.64453 11.75L11.1659 15.25L18.2088 8.25"
+      stroke="#939AA2"
+      stroke-width="1.5"
+      stroke-linecap="square"
+      stroke-linejoin="round"
+    />
+  </g>
+  <defs>
+    <clipPath id="clip0_19_17599">
+      <rect
+        width="24.1468"
+        height="24"
+        fill="white"
+        transform="translate(0.853516)"
+      />
+    </clipPath>
+  </defs>
+</svg>;
