f9aaed892b2ca33540ae2eff0f0b5883ee76c958
Mateusz Solecki <solmat@gmail.com>
2023-02-08 20:39:53 +0100

    update

--- web-app/src/pages/onboarding/accounts-descriptions.tsx
+++ web-app/src/pages/onboarding/accounts-descriptions.tsx
@@ -6,32 +6,29 @@ import { BlackModal } from '../../components/BlackModal';
 import { Title } from '../../components/Title';
 import { MainLayout } from '../../layouts/MainLayout';
 
-const accountTypesDescriptions = [
+const accountTypes = [
   {
     title: 'For Individuals',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    value: 'individual',
+    buttonText: 'Select Individuals',
+    value: 'indyviduals',
   },
   {
     title: 'For Corporations',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,  adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    value: 'corporation',
+    buttonText: 'Select Corporations',
+    value: 'corporations',
   },
   {
     title: 'For Trust',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,  adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    buttonText: 'Select Trusts',
     value: 'trust',
   },
 ];
-
-const accountTypesSelectTitles = new Map<string, string>([
-  ['individual', 'Select Individuals'],
-  ['corporation', 'Select Corporations'],
-  ['trust', 'Select Trusts'],
-]);
 
 const AccountsDescriptionsPage: NextPage = () => {
   return (
@@ -47,21 +44,21 @@ const AccountsDescriptionsPage: NextPage = () => {
           subtitle="Lear more about each option below"
         />
 
-        <RadioGroup className="mt-60 flex flex-col gap-15 text-white lg:mt-0 lg:flex-row">
-          {accountTypesDescriptions.map(accountType => (
+        <RadioGroup className="mb-30 flex flex-col items-center gap-24">
+          {accountTypes.map(account => (
             <div
-              key={accountType.value}
-              className="flex w-full flex-col justify-between border border-white"
+              key={account.value}
+              className="border border-white"
             >
               <div className="flex flex-col gap-12 p-20">
-                <Typography variant="h6">{accountType.title}</Typography>
-                <Typography variant="paragraph">{accountType.description}</Typography>
+                <Typography variant="paragraph-large">{account.title}</Typography>
+                <Typography variant="paragraph-small">{account.description}</Typography>
               </div>
               <RadioGroupItem
-                value={accountType.value}
+                value={account.value}
                 className="w-full bg-green-frost-01 py-20 text-paragraph-emphasized text-black-01"
               >
-                {accountTypesSelectTitles.get(accountType.value)}
+                {account.buttonText}
               </RadioGroupItem>
             </div>
           ))}
