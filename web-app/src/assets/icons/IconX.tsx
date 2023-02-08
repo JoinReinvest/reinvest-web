bacd88c10084cb8bec9ff401ddd6c062cadd5de7
Reinny Almonte <reinny@hookooekoo.co>
2023-02-08 09:51:18 -0400

    feat: adding icon asset "X"

---
+++ web-app/src/assets/icons/IconX.tsx
@@ -0,0 +1,26 @@
+import { Icon } from '@hookooekoo/ui-icon';
+
+export const IconX = Icon(
+  <svg
+    width="29"
+    height="29"
+    viewBox="0 0 29 29"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M27 2L2 27"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="square"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M2 2L27 27"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="square"
+      strokeLinejoin="round"
+    />
+  </svg>,
+);
