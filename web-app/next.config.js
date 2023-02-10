cd137dc33a36e34030e4fbb3534151a77cb1f645
adammalej <adammalej1@gmail.com>
2023-02-10 09:10:44 +0100

    feat: upgrade next config

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -5,21 +5,22 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const withVideos = require('next-videos');
+
 /**
  * @type {import('next').NextConfig}
  **/
-module.exports = withBundleAnalyzer({
-  eslint: {
-    dirs: ['src'],
-  },
-  poweredByHeader: false,
-  trailingSlash: true,
-  reactStrictMode: true,
-  images: {},
-  env: {
-    SITE_NAME: process.env.SITE_NAME,
-  },
-});
-
-const withVideos = require('next-videos');
-module.exports = withVideos();
+module.exports = withVideos(
+  withBundleAnalyzer({
+    eslint: {
+      dirs: ['src'],
+    },
+    poweredByHeader: false,
+    trailingSlash: true,
+    reactStrictMode: true,
+    images: {},
+    env: {
+      SITE_NAME: process.env.SITE_NAME,
+    },
+  }),
+);
