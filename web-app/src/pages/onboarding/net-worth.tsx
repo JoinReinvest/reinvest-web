7c3392381b9e4bed2de5b3df14ffcfedd2aa9e9b
Reinny Almonte <reinny@hookooekoo.co>
2023-02-13 09:41:34 -0400

    feat: defining onboarding net-worth view

---
+++ web-app/src/pages/onboarding/net-worth.tsx
@@ -0,0 +1,74 @@
+import { BlackModal } from 'components/BlackModal';
+import { Link } from 'components/Link';
+import { Select } from 'components/Select';
+import { Title } from 'components/Title';
+import { MainLayout } from 'layouts/MainLayout';
+import { NextPage } from 'next';
+import { ComponentProps, useEffect, useState } from 'react';
+
+const NET_OPTIONS: ComponentProps<typeof Select>['options'] = [
+  '$25,000 - $50,000',
+  '$50,000 - $75,000',
+  '$75,000 - $100,000',
+  '$100,000 - $125,000',
+  '$125,000 - $150,000',
+  '$150,000 - $175,000',
+  '$175,000 - $200,000',
+].map(item => ({ label: item, value: item.split(' ').join('') }));
+
+const CheckPhonePage: NextPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [formFields, setFormFields] = useState({
+    netIncome: '',
+    netWorth: '',
+  });
+
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
+
+  const updateField = (fields: Partial<typeof formFields>) => {
+    setFormFields({ ...formFields, ...fields });
+  };
+
+  return (
+    <MainLayout>
+      <BlackModal isOpen={isOpen}>
+        <Title title="What is approximate net worth and income?" />
+
+        <div className="flex flex-col gap-24">
+          <Select
+            name="net-income"
+            value={formFields.netIncome}
+            onChange={option => {
+              updateField({ netIncome: option?.value });
+            }}
+            options={NET_OPTIONS}
+            placeholder="Net Income"
+            required
+          />
+
+          <Select
+            name="net-worth"
+            value={formFields.netWorth}
+            onChange={option => {
+              updateField({ netWorth: option?.value });
+            }}
+            options={NET_OPTIONS}
+            placeholder="Net Worth"
+            required
+          />
+
+          <Link
+            title="Why is this information required?"
+            href="/"
+          >
+            Required. Why?
+          </Link>
+        </div>
+      </BlackModal>
+    </MainLayout>
+  );
+};
+
+export default CheckPhonePage;
