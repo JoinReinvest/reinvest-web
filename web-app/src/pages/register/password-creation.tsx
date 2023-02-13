94c5aca81b0e2da75fe2ae73256661355b5c1e8e
adammalej <adammalej1@gmail.com>
2023-02-13 12:06:49 +0100

    feat: upgrade password-creation page

--- web-app/src/pages/register/password-creation.tsx
+++ web-app/src/pages/register/password-creation.tsx
@@ -1,19 +1,42 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ConfirmPasswordInput } from 'components/FormElements/ConfirmPasswordInput';
+import { PasswordInput } from 'components/FormElements/PasswordInput';
 import { NextPage } from 'next';
 import { useEffect, useState } from 'react';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import zod, { Schema } from 'zod';
 
 import { BlackModal } from '../../components/BlackModal';
-import { PasswordInput } from '../../components/FormElements/PasswordInput';
-import { WhyRequiredLink } from '../../components/Links/WhyRequiredLink';
 import { PasswordChecklist } from '../../components/PasswordChecklist';
 import { Title } from '../../components/Title';
+import { formValidationRules } from '../../formValidationRules';
 import { MainLayout } from '../../layouts/MainLayout';
+
+export interface PasswordCreationFields {
+  email: string;
+  password: string;
+}
+
+export const formSchema: Schema<PasswordCreationFields> = zod.object({
+  email: formValidationRules.email,
+  password: formValidationRules.password,
+});
 
 const PasswordCreationPage: NextPage = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const form = useForm<PasswordCreationFields>({
+    resolver: zodResolver(formSchema),
+  });
+
   useEffect(() => {
     setIsOpen(true);
   }, []);
+
+  const onSubmit: SubmitHandler<PasswordCreationFields> = async () => {
+    console.log(form.getValues('email')); // eslint-disable-line
+    console.log(form.getValues('password')); // eslint-disable-line
+  };
 
   return (
     <MainLayout>
@@ -23,12 +46,21 @@ const PasswordCreationPage: NextPage = () => {
           subtitle="Create a unique password for your account to continue."
         />
 
-        <PasswordInput
-          onChange={console.log}// eslint-disable-line
-          value={'email@example.com'}
-        />
-
-        <WhyRequiredLink href="/" />
+        <FormProvider {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="max-w-330 z-30 flex flex-col items-center justify-center gap-16"
+          >
+            <PasswordInput
+              onChange={console.log} // eslint-disable-line
+              value={'email@example.com'}
+            />
+            <ConfirmPasswordInput
+              onChange={console.log} // eslint-disable-line
+              value={'email@example.com'}
+            />
+          </form>
+        </FormProvider>
 
         <PasswordChecklist
           password={'test'}
