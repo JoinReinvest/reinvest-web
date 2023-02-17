0613c02cc4de1cda2ea31942adaf8e1b9127d720
adammalej <adammalej1@gmail.com>
2023-02-17 10:30:58 +0100

    feat: add getIndividualDraftAccount

---
+++ web-app/src/services/getIndividualDraftAccount.ts
@@ -0,0 +1,40 @@
+import { useQuery } from '@tanstack/react-query';
+import { gql } from 'graphql-request';
+
+import { GraphQLClient } from './GraphQLClient';
+
+export const getIndividualDraftAccountQuery = gql`
+  query getIndividualDraftAccount($accountId: ID) {
+    getIndividualDraftAccount(accountId: $accountId) {
+      id
+      experience
+      employmentStatus
+      employer {
+        nameOfEmployer
+        occupation
+        industry
+      }
+      netWorth {
+        from
+        to
+      }
+      netIncome {
+        from
+        to
+      }
+    }
+  }
+`;
+
+export const useGetIndividualDraftAccount = (accountId: string) => {
+  const graphQLClient = GraphQLClient();
+
+  return useQuery({
+    queryKey: ['getIndividualDraftAccount', accountId],
+    queryFn: async () => {
+      const { getIndividualDraftAccount } = await graphQLClient.request(getIndividualDraftAccountQuery);
+
+      return getIndividualDraftAccount;
+    },
+  });
+};
