e2a30e42219b9e6f51200f6baf3208e441a34eaf
adammalej <adammalej1@gmail.com>
2023-02-17 10:24:32 +0100

    feat: add getAccount hook

---
+++ web-app/src/services/getAccount.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import { gql } from 'graphql-request';
+
+import { GraphQLClient } from './GraphQLClient';
+
+export const getAccountQuery = gql`
+  query getAccount($accountId: String) {
+    getAccount(accountId: $accountId) {
+      id
+      type
+    }
+  }
+`;
+
+export const useGetAccount = (accountId: string) => {
+  const graphQLClient = GraphQLClient();
+
+  return useQuery({
+    queryKey: ['getAccount', accountId],
+    queryFn: async () => {
+      const { getAccount } = await graphQLClient.request(getAccountQuery);
+
+      return getAccount;
+    },
+  });
+};
