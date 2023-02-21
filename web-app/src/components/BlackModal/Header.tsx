81327fab5699c6c1d5972e334f6e1b1851bb040c
adammalej <adammalej1@gmail.com>
2023-02-21 11:34:04 +0100

    feat: upgrade header

--- web-app/src/components/BlackModal/Header.tsx
+++ web-app/src/components/BlackModal/Header.tsx
@@ -3,13 +3,12 @@ import { IconArrowLeft } from 'assets/icons/IconArrowLeft';
 import { Logo } from 'assets/Logo';
 
 export const Header = () => (
-  <header className="w-full">
-    <div className="l-20 t-30 absolute">
+  <header className="relative flex w-full items-center justify-center">
+    <div className="absolute left-0 top-1/2 -translate-y-1/2">
       <DialogClose>
         <IconArrowLeft className="stroke-white" />
       </DialogClose>
     </div>
-
-    <Logo className="mx-auto fill-white" />
+    <Logo className="fill-white" />
   </header>
 );
