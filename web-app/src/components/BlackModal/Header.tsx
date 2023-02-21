77a441712c3ba214cd0b09106c89134205daa4eb
adammalej <adammalej1@gmail.com>
2023-02-21 11:37:50 +0100

    feat: upgrade header close button style

--- web-app/src/components/BlackModal/Header.tsx
+++ web-app/src/components/BlackModal/Header.tsx
@@ -3,12 +3,10 @@ import { IconArrowLeft } from 'assets/icons/IconArrowLeft';
 import { Logo } from 'assets/Logo';
 
 export const Header = () => (
-  <header className="relative flex w-full items-center justify-center">
-    <div className="absolute left-0 top-1/2 -translate-y-1/2">
-      <DialogClose>
-        <IconArrowLeft className="stroke-white" />
-      </DialogClose>
-    </div>
+  <header className="black-modal-header relative flex w-full items-center justify-center">
+    <DialogClose>
+      <IconArrowLeft className="stroke-white" />
+    </DialogClose>
     <Logo className="fill-white" />
   </header>
 );
