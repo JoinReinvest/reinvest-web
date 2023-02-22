f475916c225b5169be4e1d1df65407727453144a
adammalej <adammalej1@gmail.com>
2023-02-22 11:22:19 +0100

    feat: create 404 page

--- web-app/src/pages/404.tsx
+++ web-app/src/pages/404.tsx
@@ -1,11 +1,26 @@
-import { MainLayout } from 'layouts/MainLayout';
-import React from 'react';
+import { Link } from 'components/Link';
+import { Typography } from 'components/Typography';
 
 const index = () => {
   return (
-    <MainLayout>
-      <section>404</section>
-    </MainLayout>
+    <div className="p-30 flex h-screen flex-col items-center justify-center gap-60">
+      <div>
+        <Typography
+          variant="h1"
+          className="text-center"
+        >
+          404
+          <span className="mx-20">|</span>
+          Page not found
+        </Typography>
+      </div>
+      <Link
+        title="Go to login page"
+        href="/login"
+      >
+        Go to login page
+      </Link>
+    </div>
   );
 };
 
