201ffcb3b9876e5a631161c33c171884215ff526
MalejAdam <adammalej1@gmail.com>
2023-02-23 17:10:45 +0100

    feat: use greaphql error

--- web-app/src/services/getApiClient.ts
+++ web-app/src/services/getApiClient.ts
@@ -1,9 +1,10 @@
 import { env } from 'env';
+import { GraphQLError } from 'graphql';
 import { GraphQLClient } from 'graphql-request';
 
 export const getApiClient = (token: string | undefined) => {
   if (!token) {
-    throw new Error('Empty token');
+    throw new GraphQLError('Empty token');
   }
 
   return new GraphQLClient(env.apiUrl, {
