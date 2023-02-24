09142c1574b881678b53cc84cec7ed43fdb76bfd
Reinny Almonte <reinny@hookooekoo.co>
2023-02-24 14:48:05 -0400

    style: defining LoadingOverlay for loading states

--- src/views/register/flow-steps/email.tsx
+++ src/views/register/flow-steps/email.tsx
@@ -1,10 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from 'components/Button';
 import { InputEmail } from 'components/FormElements/InputEmail';
+import { LoadingOverlay } from 'components/LoadingOverlay';
 import { Typography } from 'components/Typography';
 import { URL } from 'constants/urls';
 import { formValidationRules } from 'formValidationRules';
 import Link from 'next/link';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { StepComponentProps, StepParams } from 'services/form-flow';
 import zod, { Schema } from 'zod';
@@ -15,6 +17,8 @@ type Fields = Pick<RegisterFormFields, 'email'>;
 
 export const StepEmail: StepParams<RegisterFormFields> = {
   Component: ({ storeFields, updateStoreFields, moveToNextStep }: StepComponentProps<RegisterFormFields>) => {
+    const [isValidatingEmail, setIsValidatingEmail] = useState(false);
+
     const schema: Schema<Fields> = zod.object({
       email: formValidationRules.email,
     });
@@ -22,43 +26,51 @@ export const StepEmail: StepParams<RegisterFormFields> = {
     const { handleSubmit, control, formState } = useForm<Fields>({ defaultValues: storeFields, resolver: zodResolver(schema) });
     const shouldButtonBeDisabled = !formState.isValid || formState.isSubmitting;
 
-    const onSubmit: SubmitHandler<Fields> = fields => {
+    const onSubmit: SubmitHandler<Fields> = async fields => {
       // TO-DO: Validate that the e-mail is not being used
       //    by another user - if so, display an error on the input
       //    otherwise, call `moveToNextStep()`.
-
-      updateStoreFields(fields);
-      moveToNextStep();
+      try {
+        setIsValidatingEmail(true);
+        updateStoreFields(fields);
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        setIsValidatingEmail(false);
+        moveToNextStep();
+      } catch (error) {
+        setIsValidatingEmail(false);
+      }
     };
 
     return (
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="register-form z-30 flex w-full max-w-330 flex-col items-center justify-center gap-16"
-      >
-        <Typography variant="h2">Sign up</Typography>
-        <Typography variant="paragraph-large">Enter your email below to get started.</Typography>
-
-        <InputEmail
-          name="email"
-          control={control}
-          required
-        />
-
-        <Link
-          href={URL.login}
-          className="typo-paragraph-large"
+      <LoadingOverlay display={isValidatingEmail}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="register-form z-30 flex w-full max-w-330 flex-col items-center justify-center gap-16"
         >
-          Already have an account?
-        </Link>
+          <Typography variant="h2">Sign up</Typography>
+          <Typography variant="paragraph-large">Enter your email below to get started.</Typography>
 
-        <Button
-          type="submit"
-          label="Sign Up"
-          variant="default"
-          disabled={shouldButtonBeDisabled}
-        />
-      </form>
+          <InputEmail
+            name="email"
+            control={control}
+            required
+          />
+
+          <Link
+            href={URL.login}
+            className="typo-paragraph-large"
+          >
+            Already have an account?
+          </Link>
+
+          <Button
+            type="submit"
+            label="Sign Up"
+            variant="default"
+            disabled={shouldButtonBeDisabled}
+          />
+        </form>
+      </LoadingOverlay>
     );
   },
 };
--- src/views/register/flow-steps/referral-code.tsx
+++ src/views/register/flow-steps/referral-code.tsx
@@ -2,12 +2,14 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from 'components/Button';
 import { Form } from 'components/FormElements/Form';
 import { InputReferralCode } from 'components/FormElements/InputReferralCode';
+import { LoadingOverlay } from 'components/LoadingOverlay';
 import { Title } from 'components/Title';
+import { formValidationRules } from 'formValidationRules';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { StepComponentProps, StepParams } from 'services/form-flow';
 import zod, { Schema } from 'zod';
 
-import { formValidationRules } from '../../../formValidationRules';
 import { RegisterFormFields } from '../form-fields';
 
 type Fields = Pick<RegisterFormFields, 'referralCode'>;
@@ -16,6 +18,8 @@ export const StepReferralCode: StepParams<RegisterFormFields> = {
   doesMeetConditionFields: fields => !!fields.email,
 
   Component: ({ storeFields, updateStoreFields, moveToNextStep }: StepComponentProps<RegisterFormFields>) => {
+    const [isValidatingReferralCode, setIsValidatingReferralCode] = useState(false);
+
     const schema: Schema<Fields> = zod.object({
       referralCode: formValidationRules.referralCode,
     });
@@ -24,11 +28,21 @@ export const StepReferralCode: StepParams<RegisterFormFields> = {
 
     const shouldButtonBeDisabled = !formState.isValid || formState.isSubmitting;
 
-    const onSubmit: SubmitHandler<Fields> = fields => {
+    const onSubmit: SubmitHandler<Fields> = async fields => {
       // TO-DO: Check if the referral code actually pertains to another
       //    user - if so, proceed call `updateStoreFields(fields)` so that
       //    the next step will be the confirmation message once
       //    `moveToNextStep()` is invoked.
+      try {
+        setIsValidatingReferralCode(true);
+        updateStoreFields(fields);
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        setIsValidatingReferralCode(false);
+        moveToNextStep();
+      } catch (error) {
+        setIsValidatingReferralCode(false);
+      }
+
       updateStoreFields(fields);
       moveToNextStep();
     };
@@ -38,33 +52,35 @@ export const StepReferralCode: StepParams<RegisterFormFields> = {
     };
 
     return (
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Title
-          title="Do you have a referral code? (optional)"
-          subtitle="You and your referrer will receive $20 in dividends following your first investment!"
-        />
-
-        <InputReferralCode
-          name="referralCode"
-          control={control}
-        />
-
-        <div className="absolute bottom-0 w-full md:relative md:bottom-auto">
-          <Button
-            type="button"
-            variant="outlined"
-            label="Skip"
-            onClick={onSkip}
-            className="mb-16 text-white"
+      <LoadingOverlay display={isValidatingReferralCode}>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Title
+            title="Do you have a referral code? (optional)"
+            subtitle="You and your referrer will receive $20 in dividends following your first investment!"
           />
 
-          <Button
-            type="submit"
-            label="Enter Code"
-            disabled={shouldButtonBeDisabled}
+          <InputReferralCode
+            name="referralCode"
+            control={control}
           />
-        </div>
-      </Form>
+
+          <div className="absolute bottom-0 w-full md:relative md:bottom-auto">
+            <Button
+              type="button"
+              variant="outlined"
+              label="Skip"
+              onClick={onSkip}
+              className="mb-16 text-white"
+            />
+
+            <Button
+              type="submit"
+              label="Enter Code"
+              disabled={shouldButtonBeDisabled}
+            />
+          </div>
+        </Form>
+      </LoadingOverlay>
     );
   },
 };
