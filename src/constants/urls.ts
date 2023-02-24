e875367b774bb49379009a364cad733557408167
Reinny Almonte <reinny@hookooekoo.co>
2023-02-24 11:29:53 -0400

    feat: linking forgot-page on login page

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -2,7 +2,7 @@ export const URL = {
   login: '/login',
   logout: '/logout',
   register: '/register',
-  forgot_password: '/forgot_password',
+  forgot_password: '/forgot-password',
   terms_conditions: '/terms-and-conditions',
   privacy_policy: '/privacy-policy',
 };
