4ed73d7ade3e7d9a279bdb33e371d9485a61b10d
AdamHKEK <adam@hookooekoo.co>
2023-02-28 08:40:11 +0100

    feat: start initiated reset password request to aws cognito

--- src/views/forgot-password/flow-steps/email.tsx
+++ src/views/forgot-password/flow-steps/email.tsx
@@ -3,8 +3,11 @@ import { Button } from 'components/Button';
 import { Form } from 'components/FormElements/Form';
 import { InputEmail } from 'components/FormElements/InputEmail';
 import { Title } from 'components/Title';
+import { Typography } from 'components/Typography';
 import { formValidationRules } from 'formValidationRules';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { forgotPassword } from 'services/auth/forgotPassword';
 import { StepComponentProps, StepParams } from 'services/form-flow';
 import zod, { Schema } from 'zod';
 
@@ -17,6 +20,8 @@ export const StepEmail: StepParams<ForgotPasswordFormFields> = {
     const schema: Schema<Fields> = zod.object({
       email: formValidationRules.email,
     });
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
 
     const { handleSubmit, control, formState } = useForm<Fields>({ defaultValues: storeFields, resolver: zodResolver(schema) });
     const shouldButtonBeDisabled = !formState.isValid || formState.isSubmitting;
@@ -25,9 +30,18 @@ export const StepEmail: StepParams<ForgotPasswordFormFields> = {
       // TO-DO: Validate that the email pertains to an user
       //    - if so proceed to the next step by calling
       //    `moveToNextStep()`, otherwise display an error
+      setIsLoading(true);
+      const result = await forgotPassword(fields.email);
 
       updateStoreFields(fields);
-      moveToNextStep();
+
+      if (result === 'SUCCESS') {
+        moveToNextStep();
+      } else {
+        setError(result);
+      }
+
+      setIsLoading(false);
     };
 
     return (
@@ -36,6 +50,15 @@ export const StepEmail: StepParams<ForgotPasswordFormFields> = {
           title="Reset Password"
           subtitle="Enter the email associated with your account and we’ll send an email with instructions to reset your password."
         />
+
+        {error && (
+          <Typography
+            variant="paragraph-large"
+            className="mb-12 text-tertiary-error"
+          >
+            {error}
+          </Typography>
+        )}
 
         <InputEmail
           control={control}
@@ -47,6 +70,7 @@ export const StepEmail: StepParams<ForgotPasswordFormFields> = {
           type="submit"
           label="Continue"
           disabled={shouldButtonBeDisabled}
+          loading={isLoading}
         />
       </Form>
     );
