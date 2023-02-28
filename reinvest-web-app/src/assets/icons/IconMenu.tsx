19cf5909e80ee36e7275a8e72c1c854a8cdddfba
Reinny Almonte <reinny@hookooekoo.co>
2023-02-28 16:57:22 -0400

    feat: icon asset for Menu

---
+++ reinvest-web-app/src/assets/icons/IconMenu.tsx
@@ -0,0 +1,35 @@
+import { Icon } from '@hookooekoo/ui-icon';
+
+export const IconMenu = Icon(
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <line
+      x1="7"
+      y1="9.25"
+      x2="25"
+      y2="9.25"
+      stroke="currentColor"
+      strokeWidth="1.5"
+    />
+    <line
+      x1="7"
+      y1="15.25"
+      x2="22"
+      y2="15.25"
+      stroke="currentColor"
+      strokeWidth="1.5"
+    />
+    <line
+      x1="7"
+      y1="21.25"
+      x2="25"
+      y2="21.25"
+      stroke="currentColor"
+      strokeWidth="1.5"
+    />
+  </svg>,
+);
