1762722b3ef66712f81510963263866eadea9315
Reinny Almonte <reinny@hookooekoo.co>
2023-03-01 11:41:30 -0400

    refactor: deprecating @hookooekoo/ui-button

--- src/components/ButtonAdd.tsx
+++ src/components/ButtonAdd.tsx
@@ -1,14 +1,17 @@
-import { Button as PrimitiveButton, ButtonProps as PrimitiveProps } from '@hookooekoo/ui-button';
 import { IconAdd } from 'assets/icons/IconAdd';
 import { Typography } from 'components/Typography';
+import { ButtonHTMLAttributes } from 'react';
 
-interface Props extends Pick<PrimitiveProps, 'onClick' | 'disabled'> {
+interface Props extends PrimitiveProps {
   subtitle: string;
   title: string;
 }
 
+type PrimitiveProps = Pick<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick' | 'disabled'>;
+
 export const ButtonAdd = ({ title, subtitle, onClick, disabled = false }: Props) => (
-  <PrimitiveButton
+  <button
+    type="button"
     className="flex items-center gap-x-22 border border-gray-04 p-22 text-left"
     onClick={onClick}
     disabled={disabled}
@@ -24,5 +27,5 @@ export const ButtonAdd = ({ title, subtitle, onClick, disabled = false }: Props)
         {subtitle}
       </Typography>
     </div>
-  </PrimitiveButton>
+  </button>
 );
--- src/components/Notification/NotificationButton.tsx
+++ src/components/Notification/NotificationButton.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@hookooekoo/ui-button';
 import { Typography } from 'components/Typography';
 
 import { NotificationAction } from './interfaces';
@@ -6,11 +5,12 @@ import { NotificationAction } from './interfaces';
 type Props = NotificationAction;
 
 export const NotificationButton = ({ label, onClick, disabled = false }: Props) => (
-  <Button
+  <button
+    type="button"
     className="flex h-full w-full items-center justify-center bg-white p-15"
     onClick={onClick}
     disabled={disabled}
   >
     <Typography variant="button">{label}</Typography>
-  </Button>
+  </button>
 );
