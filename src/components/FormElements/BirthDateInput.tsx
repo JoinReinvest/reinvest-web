a13772b95282ab2025e555d1fd14e517735fed18
Reinny Almonte <126076121+reinny-hkek@users.noreply.github.com>
2023-03-01 05:23:30 -0400

    Add validation for date of birth component (#8)
    * feat: adding mask validation for InputBirthDate
    
    * refactor: deprecating use of older Input for BirthDate
    
    * chore: adding dayjs as a dependency
    
    * refactor: using dayjs to parse and format dates
    
    ---------
    
    Co-authored-by: msolecki <105009456+msolecki-hkek@users.noreply.github.com>

--- src/components/FormElements/BirthDateInput.tsx
+++
@@ -1,9 +0,0 @@
-import { InputMasked } from './InputMasked';
-
-export const BirthDateInput = () => (
-  <InputMasked
-    name="birth_date"
-    maskOptions={{ mask: '00/00/0000' }}
-    placeholder="Date of Birth"
-  />
-);
