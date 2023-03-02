28587d7f37321da30acd54c6913dbae0c8e65170
AdamHKEK <adam@hookooekoo.co>
2023-03-02 14:37:57 +0100

    feat: upgrade dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,34 @@
 import { ProtectedPage } from 'components/ProtectedPage';
 import { Typography } from 'components/Typography';
 import { NextPage } from 'next';
+import { useGetUserProfile } from 'services/queries/getProfile';
 
 import { Link } from '../components/Link';
 import { URL } from '../constants/urls';
 import { MainLayout } from '../layouts/MainLayout';
 
+const Dashboard = () => {
+  const { data } = useGetUserProfile();
+
+  return (
+    <MainLayout>
+      <Typography variant="h3">First name: {data?.details?.firstName} </Typography>
+      <Typography variant="h3">Middle name: {data?.details?.middleName} </Typography>
+      <Typography variant="h3">Last name: {data?.details?.lastName}</Typography>
+      <Link
+        title="Logout"
+        href={URL.logout}
+      >
+        LogOut
+      </Link>
+    </MainLayout>
+  );
+};
+
 const Index: NextPage = () => {
   return (
     <ProtectedPage>
-      <MainLayout>
-        <Typography variant="h3">First name: </Typography>
-        <Typography variant="h3">Middle name: </Typography>
-        <Typography variant="h3">Last name: </Typography>
-        <Link
-          title="Logout"
-          href={URL.logout}
-        >
-          LogOut
-        </Link>
-      </MainLayout>
+      <Dashboard />
     </ProtectedPage>
   );
 };
