7abb45a4080faefbe21bf4bd301eacfab96356d5
AdamHKEK <adam@hookooekoo.co>
2023-03-03 07:35:18 +0100

    fix: use classnames to connect styles in form component

--- reinvest-web-app/src/components/FormElements/Form.tsx
+++ reinvest-web-app/src/components/FormElements/Form.tsx
@@ -1,4 +1,5 @@
 import { FormEventHandler, PropsWithChildren } from 'react';
+import cx from 'classnames';
 
 interface FormProps extends PropsWithChildren {
   className?: string;
@@ -9,7 +10,7 @@ export const Form = ({ onSubmit, children, className }: FormProps) => {
   return (
     <form
       onSubmit={onSubmit}
-      className={`relative flex h-full flex-col md:justify-center ${className}`}
+      className={cx('relative flex h-full flex-col md:justify-center', className)}
     >
       {children}
     </form>
