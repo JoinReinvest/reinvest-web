ec4e16c50b31bb6374a86212f9e7225f712d04f9
Reinny Almonte <126076121+reinny-hkek@users.noreply.github.com>
2023-03-03 03:59:10 -0400

    feat: base configuration for the onboarding flow (#29)

---
+++ reinvest-web-app/src/views/onboarding/index.tsx
@@ -0,0 +1,37 @@
+import { BlackModal } from 'components/BlackModal';
+import { URL } from 'constants/urls';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useFormFlowContext } from 'services/form-flow';
+
+export const OnboardingFlow = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
+
+  const {
+    CurrentStepView,
+    meta: { isFirstStep },
+    moveToPreviousValidStep,
+  } = useFormFlowContext();
+
+  useEffect(() => {
+    setIsModalOpen(true);
+  }, []);
+
+  const onModalClickBack = () => {
+    if (isFirstStep) {
+      router.push(URL.login);
+    } else {
+      moveToPreviousValidStep();
+    }
+  };
+
+  return (
+    <BlackModal
+      isOpen={isModalOpen}
+      onOpenChange={onModalClickBack}
+    >
+      <CurrentStepView />
+    </BlackModal>
+  );
+};
