216cb757754787f7d0ff8b138b0f73188c23b081
Reinny Almonte <reinny@hookooekoo.co>
2023-03-06 17:22:12 -0400

    feat: defining account type step for onboarding flow

---
+++ reinvest-web-app/src/views/onboarding/flow-steps/account-type.tsx
@@ -0,0 +1,120 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from 'components/Button';
+import { ButtonLink } from 'components/ButtonLink';
+import { Form } from 'components/FormElements/Form';
+import { SelectionCards } from 'components/FormElements/SelectionCards';
+import { InformationModal } from 'components/InformationModal';
+import { Title } from 'components/Title';
+import { Typography } from 'components/Typography';
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { StepComponentProps, StepParams } from 'services/form-flow';
+import { z } from 'zod';
+
+import { OnboardingFormFields } from '../form-fields';
+import { Identifiers } from '../identifiers';
+
+const ACCOUNT_TYPES = [
+  {
+    title: 'For Individuals',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consel.',
+    value: 'individual',
+  },
+  {
+    title: 'For Corporations',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consel.',
+    value: 'corporation',
+  },
+  {
+    title: 'For Trust',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consel.',
+    value: 'trust',
+  },
+];
+
+type Fields = Pick<OnboardingFormFields, 'accountType'>;
+
+const schema = z.object({
+  accountType: z.enum(['individual', 'corporation', 'trust']),
+});
+
+export const StepFullName: StepParams<OnboardingFormFields> = {
+  identifier: Identifiers.ACCOUNT_TYPE,
+
+  Component: ({ storeFields, updateStoreFields, moveToNextStep }: StepComponentProps<OnboardingFormFields>) => {
+    const [isInformationModalOpen, setIsInformationModalOpen] = useState(false);
+
+    const { handleSubmit, formState, control } = useForm<Fields>({
+      mode: 'all',
+      resolver: zodResolver(schema),
+      defaultValues: storeFields,
+    });
+
+    const shouldButtonBeDisabled = !formState.isValid || formState.isSubmitting;
+
+    const onSubmit: SubmitHandler<Fields> = fields => {
+      updateStoreFields(fields);
+      moveToNextStep();
+    };
+
+    const onLinkClick = () => {
+      setIsInformationModalOpen(true);
+    };
+
+    return (
+      <>
+        <Form
+          onSubmit={handleSubmit(onSubmit)}
+          expandsOnLargerScreen
+        >
+          <Title title="Which type of account would you like to open?" />
+
+          <SelectionCards
+            name="accountType"
+            control={control}
+            options={ACCOUNT_TYPES}
+            className="mb-30 flex items-stretch justify-center gap-24 max-lg:flex-col"
+            orientation="vertical"
+          />
+
+          <ButtonLink
+            label="Not sure which is best for you?"
+            onClick={onLinkClick}
+          />
+
+          <Button
+            type="submit"
+            disabled={shouldButtonBeDisabled}
+            label="Continue"
+          />
+        </Form>
+
+        <InformationModal
+          title="[Tooltip Item Name]"
+          isOpen={isInformationModalOpen}
+          onOpenChange={setIsInformationModalOpen}
+        >
+          <div className="flex flex-col gap-16">
+            <Typography variant="paragraph-large">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+              inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+              fugit, sed quia consequuntur magni.
+            </Typography>
+
+            <Typography variant="paragraph-large">
+              dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+              exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
+              voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Sed ut perspiciatis unde
+              omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+              architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia vol.
+            </Typography>
+          </div>
+        </InformationModal>
+      </>
+    );
+  },
+};
--- reinvest-web-app/src/views/onboarding/identifiers.ts
+++ reinvest-web-app/src/views/onboarding/identifiers.ts
@@ -1,3 +1,4 @@
 export enum Identifiers {
+  ACCOUNT_TYPE = 'ACCOUNT_TYPE',
   FULL_NAME = 'FULL_NAME',
 }
