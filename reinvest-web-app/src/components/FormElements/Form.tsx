f89749bfee5289c2d6b426c6b64c23b6de322ed9
Reinny Almonte <reinny@hookooekoo.co>
2023-03-06 17:20:20 -0400

    refactor: moving fixed width from BlackModal to Form component

--- reinvest-web-app/src/components/FormElements/Form.tsx
+++ reinvest-web-app/src/components/FormElements/Form.tsx
@@ -1,16 +1,23 @@
 import cx from 'classnames';
-import { FormEventHandler, PropsWithChildren } from 'react';
+import { HTMLAttributes, PropsWithChildren } from 'react';
 
-interface FormProps extends PropsWithChildren {
-  className?: string;
-  onSubmit?: FormEventHandler<HTMLFormElement>;
+type PrimitiveProps = Pick<HTMLAttributes<HTMLFormElement>, 'className' | 'onSubmit'>;
+interface FormProps extends PrimitiveProps, PropsWithChildren {
+  expandsOnLargerScreen?: boolean;
+  isCentered?: boolean;
 }
 
-export const Form = ({ onSubmit, children, className }: FormProps) => {
+export const Form = ({ onSubmit, children, className, expandsOnLargerScreen = false, isCentered = true }: FormProps) => {
+  const styles = cx(
+    'relative flex h-full flex-col md:justify-center max-w-330',
+    { 'lg:max-w-1044 items-center': expandsOnLargerScreen, 'mx-auto': isCentered },
+    className,
+  );
+
   return (
     <form
       onSubmit={onSubmit}
-      className={cx('relative flex h-full flex-col md:justify-center', className)}
+      className={styles}
     >
       {children}
     </form>
