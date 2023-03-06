bb50585993dea2d27f9f59490707f1a725a65eda
Reinny Almonte <reinny@hookooekoo.co>
2023-03-06 09:45:23 -0400

    feat: defiing onboarding page

---
+++ reinvest-web-app/src/pages/onboarding.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next';
+import { FormFlowProvider } from 'services/form-flow';
+import { OnboardingFlow } from 'views/onboarding';
+import { FLOW_STEPS } from 'views/onboarding/flow-steps';
+import { DEFAULT_VALUES } from 'views/onboarding/form-fields';
+
+const OnboardingPage: NextPage = () => {
+  return (
+    <FormFlowProvider
+      steps={FLOW_STEPS}
+      formFieldsInitialState={DEFAULT_VALUES}
+      isResumable
+    >
+      <OnboardingFlow />
+    </FormFlowProvider>
+  );
+};
+
+export default OnboardingPage;
