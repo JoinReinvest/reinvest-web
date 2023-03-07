2e9707d927cdf815c470bb0d327a26bebead2d89
AdamHKEK <adam@hookooekoo.co>
2023-03-07 20:03:10 +0100

    refactor: change text bigger add bigger paddings for desktop

--- reinvest-web-app/src/components/Education/Card.tsx
+++ reinvest-web-app/src/components/Education/Card.tsx
@@ -11,16 +11,21 @@ export interface EducationCardProps {
 
 export const EducationCard = ({ title, subtitle, icon, buttonText }: EducationCardProps) => {
   return (
-    <div className="bg-green-frost-01/30 flex w-full px-24 py-14">
+    <div className="bg-green-frost-01/30 flex w-full px-24 py-20 lg:p-40">
       <div className="flex w-full items-center justify-between  lg:w-1/2 lg:flex-col lg:items-start lg:justify-center lg:gap-16">
         <div className="mr-16">
           <Typography
-            variant="h6"
+            variant="h5"
             className="mb-8"
           >
             {title}
           </Typography>
-          <Typography variant="paragraph">{subtitle}</Typography>
+          <Typography
+            variant="paragraph-large"
+            className="text-gray-01"
+          >
+            {subtitle}
+          </Typography>
         </div>
         <div className="bg-green-frost-01 flex aspect-square h-36 w-36 cursor-pointer items-center justify-center rounded-full lg:w-auto lg:pl-16">
           {buttonText && (
