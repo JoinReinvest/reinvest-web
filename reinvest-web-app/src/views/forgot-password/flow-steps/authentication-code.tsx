4565e39b3688f981bbdf5dbaed42d15ccbb1c2d7
AdamHKEK <126069460+AdamHKEK@users.noreply.github.com>
2023-03-07 10:42:03 +0100

    fix: user is able to resend code under forgot password (#44)
    * fix: user is able to resend code under forgot password
    
    * fix: add grenn color for link

--- reinvest-web-app/src/views/forgot-password/flow-steps/authentication-code.tsx
+++ reinvest-web-app/src/views/forgot-password/flow-steps/authentication-code.tsx
@@ -45,8 +45,7 @@ export const StepAuthenticationCode: StepParams<ForgotPasswordFormFields> = {
 
     const resendCodeOnClick = async () => {
       try {
-        await Auth.signIn(storeFields.email, storeFields.password);
-
+        await Auth.forgotPassword(storeFields.email);
         setInfoMessage('Code has been sent');
       } catch (err) {
         setError((err as Error).message);
