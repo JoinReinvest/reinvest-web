87f5239240100eaf3fa3f77b2d15e092b29c7e0a
AdamHKEK <126069460+AdamHKEK@users.noreply.github.com>
2023-03-07 19:29:17 +0100

    Ria 397 education page visual part (#49)
    * feat: add education icons and hero image
    
    * feat: crete hero and learn about section in education page
    
    * fix: upgrade bigger hero image
    
    * feat: upload education pngs
    
    * feat: crete education blog section
    
    * fix: upgrade header styles
    
    ---------
    
    Co-authored-by: msolecki <105009456+msolecki-hkek@users.noreply.github.com>

--- reinvest-web-app/src/pages/education/index.tsx
+++ reinvest-web-app/src/pages/education/index.tsx
@@ -1,5 +1,12 @@
 import { IconChart } from 'assets/icons/Education/IconChart';
 import { IconHome } from 'assets/icons/Education/IconHome';
+import image1 from 'assets/images/education/image1.png';
+import image2 from 'assets/images/education/image2.png';
+import image3 from 'assets/images/education/image3.png';
+import image4 from 'assets/images/education/image4.png';
+import image5 from 'assets/images/education/image5.png';
+import hero from 'assets/images/education-hero-phone.png';
+import { BlogCard, BlogCardProps } from 'components/Education/BlogCard';
 import hero from 'assets/images/education-hero.png';
 import { EducationCard, EducationCardProps } from 'components/Education/Card';
 import { Typography } from 'components/Typography';
@@ -22,8 +29,48 @@ const educationCards: EducationCardProps[] = [
   },
 ];
 
+const blogCards: BlogCardProps[] = [
+  {
+    imageSrc: image1,
+    subtitle: 'with Brandon Rule',
+    title: 'Real Estate Investment 101',
+  },
+  {
+    imageSrc: image2,
+    subtitle: 'with Brandon Rule',
+    title: 'Getting Started with REINVEST',
+  },
+  {
+    imageSrc: image3,
+    subtitle: 'with Brandon Rule',
+    title: 'Getting Started with REINVEST',
+  },
+  {
+    imageSrc: image4,
+    subtitle: 'April 3th, 2022',
+    title: 'Project update lorem ipsum dolor sit amet',
+  },
+  {
+    imageSrc: image5,
+    subtitle: 'April 3th, 2022',
+    title: 'Project update lorem ipsum dolor sit amet',
+  },
+  {
+    imageSrc: image5,
+    subtitle: 'April 3th, 2022',
+    title: 'Project update lorem ipsum dolor sit amet',
+  },
+];
+
 const renderCard = (card: EducationCardProps) => (
   <EducationCard
+    key={card.title}
+    {...card}
+  />
+);
+
+const renderBlogCard = (card: BlogCardProps) => (
+  <BlogCard
     key={card.title}
     {...card}
   />
@@ -53,6 +100,15 @@ const Index = () => {
           Learn About Real Estate Investing
         </Typography>
         <div className="flex flex-col gap-16 lg:flex-row">{educationCards.map(renderCard)}</div>
+      </section>
+      <section className="mb-24 lg:mb-44">
+        <Typography
+          variant="h5"
+          className="my-24"
+        >
+          Learn the basics
+        </Typography>
+        <div className="flex flex-col gap-16 lg:grid lg:grid-cols-3 lg:gap-y-36">{blogCards.map(renderBlogCard)}</div>
       </section>
     </MainLayout>
   );
