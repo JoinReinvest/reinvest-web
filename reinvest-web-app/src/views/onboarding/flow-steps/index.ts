86bc77081bf30d7db5ed9e07ec939a26d2e872a6
Reinny Almonte <reinny@hookooekoo.co>
2023-03-06 23:49:47 -0400

    feat: defining onboarding step for residency green card

--- reinvest-web-app/src/views/onboarding/flow-steps/index.ts
+++ reinvest-web-app/src/views/onboarding/flow-steps/index.ts
@@ -1,4 +1,5 @@
 import { StepFullName } from './full-name';
+import { StepResidencyGreenCard } from './residency-green-card';
 import { StepResidencyStatus } from './residency-status';
 
-export const FLOW_STEPS = [StepFullName, StepResidencyStatus];
+export const FLOW_STEPS = [StepFullName, StepResidencyStatus, StepResidencyGreenCard];
