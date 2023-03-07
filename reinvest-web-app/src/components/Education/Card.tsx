7639d691a5870de4713a38975f51260fd2f5937e
AdamHKEK <126069460+AdamHKEK@users.noreply.github.com>
2023-03-07 16:26:49 +0100

    Ria 397 education page visual part (#48)
    * feat: add education icons and hero image
    
    * feat: crete hero and learn about section in education page
    
    * fix: upgrade bigger hero image

---
+++ reinvest-web-app/src/components/Education/Card.tsx
@@ -0,0 +1,40 @@
+import { IconArrowRight } from 'assets/icons/IconArrowRight';
+import { Typography } from 'components/Typography';
+import { ReactNode } from 'react';
+
+export interface EducationCardProps {
+  icon: ReactNode;
+  subtitle: string;
+  title: string;
+  buttonText?: string;
+}
+
+export const EducationCard = ({ title, subtitle, icon, buttonText }: EducationCardProps) => {
+  return (
+    <div className="bg-green-frost-01/30 flex w-full px-24 py-14">
+      <div className="flex w-full items-center justify-between  lg:w-1/2 lg:flex-col lg:items-start lg:justify-center lg:gap-16">
+        <div className="mr-16">
+          <Typography
+            variant="h6"
+            className="mb-8"
+          >
+            {title}
+          </Typography>
+          <Typography variant="paragraph">{subtitle}</Typography>
+        </div>
+        <div className="bg-green-frost-01 flex aspect-square h-36 w-36 cursor-pointer items-center justify-center rounded-full lg:w-auto lg:pl-16">
+          {buttonText && (
+            <Typography
+              variant="paragraph"
+              className="hidden whitespace-nowrap lg:block"
+            >
+              {buttonText}
+            </Typography>
+          )}
+          <IconArrowRight />
+        </div>
+      </div>
+      {icon && <div className="hidden w-1/2 lg:flex lg:justify-end">{icon}</div>}
+    </div>
+  );
+};
