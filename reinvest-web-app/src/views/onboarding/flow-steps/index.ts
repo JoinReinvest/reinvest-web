c99986fa6adc5d88e7e2838ae3b9427eece5c29f
Reinny Almonte <reinny@hookooekoo.co>
2023-03-06 23:21:27 -0400

    feat: defining onboarding step for residency status

--- reinvest-web-app/src/views/onboarding/flow-steps/index.ts
+++ reinvest-web-app/src/views/onboarding/flow-steps/index.ts
@@ -1,3 +1,4 @@
 import { StepFullName } from './full-name';
+import { StepResidencyStatus } from './residency-status';
 
-export const FLOW_STEPS = [StepFullName];
+export const FLOW_STEPS = [StepFullName, StepResidencyStatus];
--- reinvest-web-app/src/views/onboarding/identifiers.ts
+++ reinvest-web-app/src/views/onboarding/identifiers.ts
@@ -1,3 +1,4 @@
 export enum Identifiers {
   FULL_NAME = 'FULL_NAME',
+  RESIDENCY_STATUS = 'RESIDENCY_STATUS',
 }
