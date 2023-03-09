7c6dc46ad0165c016096db4ee634665710df1fda
Reinny Almonte <reinny@hookooekoo.co>
2023-03-09 12:08:01 -0400

    feat: defining onboarding step for compliances

--- reinvest-web-app/src/views/onboarding/form-fields.ts
+++ reinvest-web-app/src/views/onboarding/form-fields.ts
@@ -27,14 +27,15 @@ export interface OnboardingFormFields {
 
   firstName?: string;
 
-  household?: {
-    // Are you or anyone in your immediate household, or, for any non-natural person, any officers, directors, or any person that owns or controls 5% (or greater) of the equity, associated with a FINRA member, organization, or the SEC.
+  compliances?: {
+    // Are you or anyone in your immediate compliances, or, for any non-natural person, any officers, directors, or any person that owns or controls 5% (or greater) of the equity, associated with a FINRA member, organization, or the SEC.
     isAssociatedWithFinra?: boolean;
-    // Are you or anyone in your household or immediate family, or, for any non-natural person, any of its directors, trustees, 10% (or more) equity holder, an officer, or member of the board of directors of a publicly traded company?
+    // Are you or anyone in your compliances or immediate family, or, for any non-natural person, any of its directors, trustees, 10% (or more) equity holder, an officer, or member of the board of directors of a publicly traded company?
     isAssociatedWithPubliclyTradedCompany?: boolean;
     // Are you or any of your immediate family a senior political figure?
     isSeniorPoliticalFigure?: boolean;
   };
+
   lastName?: string;
   middleName?: string;
   netIncome?: string;
@@ -44,12 +45,12 @@ export interface OnboardingFormFields {
 
   profilePicture?: string;
 
-  // If `household.isAssociatedWithPubliclyTradedCompany` is true
+  // If `compliances.isAssociatedWithPubliclyTradedCompany` is true
   seniorPoliticalFigure?: string;
 
-  // If `household.isSeniorPoliticalFigure` is true
+  // If `compliances.isSeniorPoliticalFigure` is true
   socialSecurityNumber?: string;
-  // If `household.isAssociatedWithFinra` is true
+  // If `compliances.isAssociatedWithFinra` is true
   tickerSymbols?: string[];
 
   visaType?: 'F-1' | 'H-1B' | 'L-1' | 'O-1' | 'G-4'; // Are we displaying this as an URL or a file upload?
@@ -58,5 +59,5 @@ export interface OnboardingFormFields {
 export const DEFAULT_VALUES: OnboardingFormFields = {
   accountType: '',
   employment: {},
-  residency: undefined,
+  residency: undefined
 };
