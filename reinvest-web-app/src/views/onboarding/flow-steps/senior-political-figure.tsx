1bc36c02a058114d2d183130a4824600abd24b5b
Reinny Almonte <reinny@hookooekoo.co>
2023-03-09 16:30:12 -0400

    feat: defining onboarding step for senior political figure

---
+++ reinvest-web-app/src/views/onboarding/flow-steps/senior-political-figure.tsx
@@ -0,0 +1,54 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from 'components/Button';
+import { Form } from 'components/FormElements/Form';
+import { TextArea } from 'components/FormElements/TextArea';
+import { Title } from 'components/Title';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { StepComponentProps, StepParams } from 'services/form-flow';
+import { z } from 'zod';
+
+import { OnboardingFormFields } from '../form-fields';
+import { Identifiers } from '../identifiers';
+
+type Fields = Pick<OnboardingFormFields, 'seniorPoliticalFigure'>;
+
+const schema = z.object({
+  seniorPoliticalFigure: z.string().min(1),
+});
+
+export const StepSeniorPoliticalFigure: StepParams<OnboardingFormFields> = {
+  identifier: Identifiers.SENIOR_POLITICAL_FIGURE,
+
+  Component: ({ storeFields, updateStoreFields, moveToNextStep }: StepComponentProps<OnboardingFormFields>) => {
+    const { control, formState, handleSubmit } = useForm<Fields>({
+      mode: 'all',
+      resolver: zodResolver(schema),
+      defaultValues: storeFields,
+    });
+
+    const shouldButtonBeDisabled = !formState.isValid || formState.isSubmitting;
+
+    const onSubmit: SubmitHandler<Fields> = fields => {
+      updateStoreFields(fields);
+      moveToNextStep();
+    };
+
+    return (
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Title title="Please provide the name and position of this senior political figure." />
+
+        <TextArea
+          name="seniorPoliticalFigure"
+          control={control}
+          maxCharacters={220}
+        />
+
+        <Button
+          type="submit"
+          label="Continue"
+          disabled={shouldButtonBeDisabled}
+        />
+      </Form>
+    );
+  },
+};
--- reinvest-web-app/src/views/onboarding/form-fields.ts
+++ reinvest-web-app/src/views/onboarding/form-fields.ts
@@ -19,14 +19,6 @@ export interface OnboardingFormFields {
   birthCountry?: string;
   // These fields are only required if residency is 'green-card' or 'visa'
   citizenshipCountry?: string;
-  dateOfBirth?: Date;
-
-  documentForVerification?: string;
-  employmentStatus?: 'employed' | 'unemployed' | 'retired' | 'student';
-  finraInstitution?: string;
-
-  firstName?: string;
-
   compliances?: {
     // Are you or anyone in your immediate compliances, or, for any non-natural person, any officers, directors, or any person that owns or controls 5% (or greater) of the equity, associated with a FINRA member, organization, or the SEC.
     isAssociatedWithFinra?: boolean;
@@ -35,6 +27,14 @@ export interface OnboardingFormFields {
     // Are you or any of your immediate family a senior political figure?
     isSeniorPoliticalFigure?: boolean;
   };
+
+  dateOfBirth?: Date;
+  documentForVerification?: string;
+  employmentStatus?: 'employed' | 'unemployed' | 'retired' | 'student';
+
+  finraInstitution?: string;
+
+  firstName?: string;
 
   lastName?: string;
   middleName?: string;
@@ -59,5 +59,5 @@ export interface OnboardingFormFields {
 export const DEFAULT_VALUES: OnboardingFormFields = {
   accountType: '',
   employment: {},
-  residency: undefined
+  residency: undefined,
 };
