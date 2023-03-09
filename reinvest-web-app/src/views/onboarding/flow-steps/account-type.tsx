8d13765825a51d71316640ce56035c3a9ab85d7c
Reinny Almonte <reinny@hookooekoo.co>
2023-03-09 08:41:39 -0400

    chore: deprecating InformationModal component

--- reinvest-web-app/src/views/onboarding/flow-steps/account-type.tsx
+++ reinvest-web-app/src/views/onboarding/flow-steps/account-type.tsx
@@ -2,10 +2,9 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from 'components/Button';
 import { Form } from 'components/FormElements/Form';
 import { SelectionCards } from 'components/FormElements/SelectionCards';
-import { InformationModal } from 'components/InformationModal';
 import { OpenModalLink } from 'components/Links/OpenModalLink';
 import { Title } from 'components/Title';
-import { Typography } from 'components/Typography';
+import { WhyRequiredBlackModal } from 'components/WhyRequiredBlackModal';
 import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { StepComponentProps, StepParams } from 'services/form-flow';
@@ -89,28 +88,10 @@ export const StepAccountType: StepParams<OnboardingFormFields> = {
           />
         </Form>
 
-        <InformationModal
-          title="[Tooltip Item Name]"
+        <WhyRequiredBlackModal
           isOpen={isInformationModalOpen}
           onOpenChange={setIsInformationModalOpen}
-        >
-          <div className="flex flex-col gap-16">
-            <Typography variant="paragraph-large">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
-              inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-              fugit, sed quia consequuntur magni.
-            </Typography>
-
-            <Typography variant="paragraph-large">
-              dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-              exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
-              voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Sed ut perspiciatis unde
-              omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-              architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia vol.
-            </Typography>
-          </div>
-        </InformationModal>
+        />
       </>
     );
   },
