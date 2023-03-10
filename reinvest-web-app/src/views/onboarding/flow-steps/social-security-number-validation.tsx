4158117c470c522070573b23ff93debce5a41a7c
Reinny Almonte <reinny@hookooekoo.co>
2023-03-10 14:16:21 -0400

    feat: displaying titles and description based on meta fields

--- reinvest-web-app/src/views/onboarding/flow-steps/social-security-number-validation.tsx
+++ reinvest-web-app/src/views/onboarding/flow-steps/social-security-number-validation.tsx
@@ -4,7 +4,7 @@ import { GetHelpLink } from 'components/Links/GetHelp';
 import { Title } from 'components/Title';
 import { Typography } from 'components/Typography';
 import { EMAILS } from 'constants/urls';
-import { StepParams } from 'services/form-flow';
+import { StepComponentProps, StepParams } from 'services/form-flow';
 
 import { OnboardingFormFields } from '../form-fields';
 import { Identifiers } from '../identifiers';
@@ -19,14 +19,29 @@ export const StepSocialSecurityNumberValidation: StepParams<OnboardingFormFields
 
   identifier: Identifiers.SOCIAL_SECURITY_NUMBER_VALIDATION,
 
-  Component: () => {
+  Component: ({ storeFields }: StepComponentProps<OnboardingFormFields>) => {
+    const { _isSocialSecurityNumberAlreadyAssigned, _isSocialSecurityNumberBanned } = storeFields;
+
+    const title = _isSocialSecurityNumberAlreadyAssigned
+      ? 'Your credentials are associated with an active REINVEST account'
+      : _isSocialSecurityNumberBanned
+      ? 'You are unable to create a REINVEST profile at this time.'
+      : '';
+
+    const issueDescription = _isSocialSecurityNumberAlreadyAssigned
+      ? 'Please login to your active account. If you believe this an error, '
+      : _isSocialSecurityNumberBanned
+      ? 'Please reach out to '
+      : '';
+
     return (
       <div>
-        <Title title="Enter your first and last name as it appears on your ID" />
+        <Title title={title} />
 
         <div className="text-center">
           <Typography variant="paragraph-large">
-            Please reach out to <GetHelpLink label={EMAILS.support} />
+            {issueDescription}
+            <GetHelpLink label={EMAILS.support} />
           </Typography>
 
           <IconXCircle className="mx-auto mt-40" />
