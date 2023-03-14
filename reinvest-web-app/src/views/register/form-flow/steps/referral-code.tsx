5b97d9c2d2d8113645a87d65c34b5dcd355c2a75
AdamHKEK <126069460+AdamHKEK@users.noreply.github.com>
2023-03-14 21:35:35 +0100

    fix: set referral code to undefined when user click skip button (#80)

--- reinvest-web-app/src/views/register/form-flow/steps/referral-code.tsx
+++ reinvest-web-app/src/views/register/form-flow/steps/referral-code.tsx
@@ -43,6 +43,7 @@ export const StepReferralCode: StepParams<RegisterFormFields> = {
     };
 
     const onSkip = () => {
+      updateStoreFields({ referralCode: undefined });
       moveToNextStep();
     };
 
