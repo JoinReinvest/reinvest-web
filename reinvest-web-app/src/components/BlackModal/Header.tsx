67aa12ef07dd3d0330f94f7a770e45e58cb5e677
Reinny Almonte <reinny@hookooekoo.co>
2023-03-14 15:24:11 -0400

    style: black modal layout

--- reinvest-web-app/src/components/BlackModal/Header.tsx
+++ reinvest-web-app/src/components/BlackModal/Header.tsx
@@ -3,7 +3,7 @@ import { IconArrowLeft } from 'assets/icons/IconArrowLeft';
 import { Logo } from 'assets/Logo';
 
 export const Header = () => (
-  <header className="black-modal-header relative flex w-full items-center justify-center">
+  <header className="black-modal-header h-32 lg:h-48 relative flex w-full items-center justify-center">
     <DialogClose>
       <IconArrowLeft className="stroke-white" />
     </DialogClose>
