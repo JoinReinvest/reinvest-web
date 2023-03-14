0f94fca326e82d8889c8b01b24b019ee01b0b8e8
Reinny Almonte <reinny@hookooekoo.co>
2023-03-14 15:01:23 -0400

    style: addressing login-layout style fixes

--- reinvest-web-app/src/layouts/LoginLayout.tsx
+++ reinvest-web-app/src/layouts/LoginLayout.tsx
@@ -7,7 +7,7 @@ export interface LoginLayoutProps {
 
 export const LoginLayout = ({ children }: LoginLayoutProps) => {
   return (
-    <div className="relative flex h-screen flex-col items-center justify-around gap-40 text-center text-white lg:justify-center lg:gap-84">
+    <div className="relative flex h-screen flex-col items-center justify-between pb-44 gap-40 text-center text-white lg:justify-center lg:gap-84">
       <div className="absolute after:absolute after:top-0 after:left-0 after:h-full after:w-full after:bg-gradient-to-b after:from-black-01/10 after:to-black-01/50">
         <video
           autoPlay
@@ -23,7 +23,7 @@ export const LoginLayout = ({ children }: LoginLayoutProps) => {
         </video>
       </div>
 
-      <LogoIcon className="z-30 mt-60 h-144 w-144 fill-green-frost-01 lg:mt-0 lg:h-96 lg:w-96" />
+      <LogoIcon className="z-30 mt-[80px] h-144 w-144 fill-green-frost-01 lg:mt-0 lg:h-96 lg:w-96" />
 
       {children}
     </div>
