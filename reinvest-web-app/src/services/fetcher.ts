4d40ec15807b5d99ad6f4ae7380df9b037c4cdce
marcin-hookooekoo <80704419+marcin-hookooekoo@users.noreply.github.com>
2023-03-20 20:23:23 +0100

    Revert "fix: token redirect (#109)" (#113)
    This reverts commit 49cd5407eaae8e40412d5c94c47d4ee1709c50ee.

--- reinvest-web-app/src/services/fetcher.ts
+++ reinvest-web-app/src/services/fetcher.ts
@@ -1,7 +1,5 @@
-import fetch from 'node-fetch';
-
-export const fetcher = (url: string, method = 'GET', body: URLSearchParams | undefined = undefined) =>
-  fetch(url, { method, body }).then(async res => {
+export const fetcher = (url: string) =>
+  fetch(url).then(async res => {
     const result = await res.json();
 
     return typeof result === 'string' ? JSON.parse(result) : result;
--- stacks/NextJsApp.ts
+++ stacks/NextJsApp.ts
@@ -3,17 +3,16 @@ import { NextjsSite, StackContext } from 'sst/constructs'
 export const NextJsApp = function NextJsApp ({ stack }: StackContext) {
     // Create the Next.js site
     const site = new NextjsSite(stack, "reinvest-web-app", {
-
         path: "reinvest-web-app/",
         // customDomain: "my-app.com",
         environment: {
-            SITE_NAME: process.env.SITE_NAME as string,
-            REINVEST_SITE_URL: process.env.REINVEST_SITE_URL as string,
-            AWS_COGNITO_USER_POOL_ID: process.env.AWS_COGNITO_USER_POOL_ID as string,
-            AWS_COGNITO_CLIENT_ID: process.env.AWS_COGNITO_CLIENT_ID as string,
-            API_URL: process.env.API_URL as string,
-            ANALYZE: process.env.ANALYZE as string,
-            AWS_COGNITO_REGION: process.env.AWS_COGNITO_REGION as string,
+            SITE_NAME: process.env.SITE_NAME,
+            REINVEST_SITE_URL: process.env.REINVEST_SITE_URL,
+            AWS_COGNITO_USER_POOL_ID: process.env.AWS_COGNITO_USER_POOL_ID,
+            AWS_COGNITO_CLIENT_ID: process.env.AWS_COGNITO_CLIENT_ID,
+            API_URL: process.env.API_URL,
+            ANALYZE: process.env.ANALYZE,
+            AWS_COGNITO_REGION: process.env.AWS_COGNITO_REGION,
         },
     })
 
