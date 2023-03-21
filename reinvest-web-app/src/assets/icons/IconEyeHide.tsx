c85621a55e0bc07251c270b13b73a139a00ffd13
Reinny Almonte <reinny@hookooekoo.co>
2023-03-21 11:23:47 -0400

    feat: icon assets for password input

---
+++ reinvest-web-app/src/assets/icons/IconEyeHide.tsx
@@ -0,0 +1,44 @@
+import { Icon } from '@hookooekoo/ui-icon';
+
+export const IconEyeHide = Icon(
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clipPath="url(#clip0_244_29131)">
+      <path
+        d="M8.77777 9L22.7778 23"
+        stroke="black"
+        strokeWidth="1.5"
+        strokeLinecap="square"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.6764 14.901C14.3845 15.1927 14.2205 15.5884 14.2203 16.001C14.2202 16.4136 14.384 16.8094 14.6757 17.1013C14.9673 17.3932 15.363 17.5573 15.7757 17.5574C16.1883 17.5576 16.5841 17.3938 16.876 17.1021"
+        stroke="black"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M13.7268 10.8395C14.3937 10.6487 15.0842 10.5531 15.7778 10.5556C18.8889 10.5556 21.4812 12.3701 23.5556 16C22.9504 17.0586 22.3018 17.9631 21.6088 18.7129M19.9443 20.1604C18.6758 21.0159 17.2882 21.4445 15.7778 21.4445C12.6667 21.4445 10.0743 19.6299 8 16C9.06478 14.1373 10.2657 12.7528 11.6027 11.8459"
+        stroke="black"
+        strokeWidth="1.5"
+        strokeLinecap="square"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_244_29131">
+        <rect
+          width="32"
+          height="32"
+          fill="white"
+        />
+      </clipPath>
+    </defs>
+  </svg>,
+);
