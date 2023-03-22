8e3e9e6bf5215c7d53707056b48540cc640c6040
Reinny Almonte <reinny@hookooekoo.co>
2023-03-21 21:07:03 -0400

    feat: addressing InputFile component layout

--- reinvest-web-app/src/components/FormElements/InputFile/UploadedFile.tsx
+++ reinvest-web-app/src/components/FormElements/InputFile/UploadedFile.tsx
@@ -1,5 +1,5 @@
-import { IconTrashcan } from '../../../assets/icons/IconTrashcan';
-import { Typography } from '../../Typography';
+import { IconTrashcan } from 'assets/icons/IconTrashcan';
+import { Typography } from 'components/Typography';
 
 interface UploadedFileProps {
   fileName: string;
@@ -7,7 +7,7 @@ interface UploadedFileProps {
 }
 export const UploadedFile = ({ fileName, onRemove }: UploadedFileProps) => {
   return (
-    <div className="flex items-center gap-8 border border-dashed border-green-frost-01 p-8">
+    <div className="flex items-center justify-between gap-8 border border-dashed border-green-frost-01 p-8">
       <Typography
         variant="button"
         className="text-left line-clamp-1"
