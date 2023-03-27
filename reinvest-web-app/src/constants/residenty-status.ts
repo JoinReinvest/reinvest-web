ef7ecf0c32ebf0688aa2e9ed9b20ba693d5d3649
AdamHKEK <adam@hookooekoo.co>
2023-03-27 11:25:17 +0200

    refactor: upgrade residency-status types

--- reinvest-web-app/src/constants/residenty-status.ts
+++ reinvest-web-app/src/constants/residenty-status.ts
@@ -13,11 +13,11 @@ export const RESIDENCY_STATUS_AS_RADIO_GROUP_OPTIONS = [
     value: DomicileType.Visa,
     title: 'Visa',
   },
-] as const;
+];
 
 type ResidencyStatusValue = (typeof RESIDENCY_STATUS_AS_RADIO_GROUP_OPTIONS)[number]['value'];
 
 export const RESIDENCY_STATUS_VALUES: [ResidencyStatusValue, ...ResidencyStatusValue[]] = [
-  RESIDENCY_STATUS_AS_RADIO_GROUP_OPTIONS[0].value,
+  RESIDENCY_STATUS_AS_RADIO_GROUP_OPTIONS[0]?.value ?? DomicileType.Citizen,
   ...RESIDENCY_STATUS_AS_RADIO_GROUP_OPTIONS.slice(1).map(({ value }) => value),
 ];
