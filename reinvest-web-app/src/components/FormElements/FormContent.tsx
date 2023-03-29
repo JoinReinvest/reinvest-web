758c5e3bac8fa7ec1890b2180b33f49ba8dc263d
Reinny Almonte <reinny@hookooekoo.co>
2023-03-29 13:42:18 -0400

    style: allowing the content of the black modals form to be scrollable

--- reinvest-web-app/src/components/FormElements/FormContent.tsx
+++ reinvest-web-app/src/components/FormElements/FormContent.tsx
@@ -5,5 +5,5 @@ type PrimitiveProps = Pick<HTMLAttributes<HTMLDivElement>, 'className'>;
 interface Props extends PrimitiveProps, PropsWithChildren {}
 
 export const FormContent = ({ className, children }: Props) => (
-  <div className={cx('flex flex-col gap-60 lg:gap-16 lg:justify-center', className)}>{children}</div>
+  <div className={cx('overflow-y-scroll flex flex-col gap-60 lg:gap-16 lg:justify-center', className)}>{children}</div>
 );
