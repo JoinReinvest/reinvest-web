e255c5f42eb44859b8fa7645a0fd360a298ccdf8
Reinny Almonte <reinny@hookooekoo.co>
2023-03-30 09:48:44 -0400

    refactor: using makeRequest instead of deprecated fetcher service

--- reinvest-web-app/src/pages/education/blog/[slug].tsx
+++ reinvest-web-app/src/pages/education/blog/[slug].tsx
@@ -1,9 +1,8 @@
+import { env } from 'env';
+import { FullWidthContentLayout } from 'layouts/FullWidthContentLayout';
 import { GetStaticPropsContext } from 'next';
-
-import { BlogPostInterface } from '../../../components/Education/BlogCard';
-import { env } from '../../../env';
-import { FullWidthContentLayout } from '../../../layouts/FullWidthContentLayout';
-import { fetcher } from '../../../services/fetcher';
+import { makeRequest } from 'services/api-request';
+import { GetPostsResponse } from 'types/site-api';
 
 export interface ArticlePageProps {
   protected: boolean;
@@ -23,9 +22,11 @@ const ArticlePage = ({ slug }: ArticlePageProps) => {
 };
 
 export async function getStaticPaths() {
-  const data = await fetcher(`${env.site.url}/api/posts`);
-
-  const paths = data ? data.data.map((article: BlogPostInterface) => ({ params: { slug: article.slug } })) : [];
+  const url = `${env.site.url}/api/posts`;
+  const response = await makeRequest<GetPostsResponse>({ url, method: 'GET' });
+  const parsedData: GetPostsResponse = typeof response.data === 'string' ? JSON.parse(response.data) : response.data;
+  const posts = parsedData.data || [];
+  const paths = posts.map(article => ({ params: { slug: article.slug } }));
 
   return {
     paths,
