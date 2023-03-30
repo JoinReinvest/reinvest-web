7fe92aa5383fdf07b94930ad0dba18f0f2d886f6
Reinny Almonte <reinny@hookooekoo.co>
2023-03-30 09:45:59 -0400

    feat: defining fetch hook

---
+++ reinvest-web-app/src/hooks/fetch.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+import useSWR, { SWRConfiguration, SWRResponse } from 'swr';
+
+type GetRequest = AxiosRequestConfig | null;
+type PrimitiveSWRResponse<Data, Error> = Pick<SWRResponse<AxiosResponse<Data>, AxiosError<Error>>, 'isValidating' | 'error' | 'mutate' | 'isLoading'>;
+type Config<Data = unknown, Error = unknown> = SWRConfiguration<AxiosResponse<Data>, AxiosError<Error>>;
+
+interface Return<Data, Error> extends PrimitiveSWRResponse<Data, Error> {
+  data: Data | undefined;
+  response: AxiosResponse<Data> | undefined;
+}
+
+export const useFetch = <Data = unknown, Error = unknown>(request: GetRequest, config: Config<Data, Error> = {}): Return<Data, Error> => {
+  const {
+    data: response,
+    error,
+    isValidating,
+    isLoading,
+    mutate,
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  } = useSWR<AxiosResponse<Data>, AxiosError<Error>>(request && JSON.stringify(request), () => axios.request<Data>(request!), {
+    ...config,
+  });
+
+  return {
+    data: response && response.data,
+    response,
+    error,
+    isValidating,
+    isLoading,
+    mutate,
+  };
+};
