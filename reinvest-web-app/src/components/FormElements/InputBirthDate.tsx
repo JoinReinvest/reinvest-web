9d9c8461b43a833e41a29d61f44332c7331d3380
Reinny Almonte <reinny@hookooekoo.co>
2023-04-06 12:58:47 -0400

    feat: changing dates validation schema from MM-DD-YYYY to MM/DD/YYYY

--- reinvest-web-app/src/components/FormElements/InputBirthDate.tsx
+++ reinvest-web-app/src/components/FormElements/InputBirthDate.tsx
@@ -1,30 +1,30 @@
 import { CustomInputMaskedProps, InputMasked } from 'components/FormElements/InputMasked';
 import dayjs from 'dayjs';
 import { FieldValues } from 'react-hook-form';
+import { DATE_FORMAT } from 'reinvest-app-common/src/constants/date-formats';
 
 type Props<FormFields extends FieldValues> = CustomInputMaskedProps<FormFields>;
 
 export function InputBirthDate<FormFields extends FieldValues>(props: Props<FormFields>) {
   const maxDate = dayjs().toDate();
   const minDate = dayjs(maxDate).subtract(100, 'years').toDate();
-  const dateFormat = 'MM-DD-YYYY';
 
   return (
     <InputMasked
       maskOptions={{
         mask: Date,
-        pattern: 'm{-}`d{-}`Y',
+        pattern: 'm{/}`d{/}`Y',
         min: minDate,
         max: maxDate,
-        format: value => dayjs(value).format(dateFormat),
-        parse: value => dayjs(value, dateFormat).toDate(),
+        format: value => dayjs(value).format(DATE_FORMAT),
+        parse: value => dayjs(value, DATE_FORMAT).toDate(),
       }}
       {...props}
       placeholder="Date of Birth"
       willTriggerChangeOnCompletion
       willUseUnmaskedValue={false}
       willTriggerChangeOnAccept={false}
-      inputPlaceholder="MM/DD/YYYY"
+      inputPlaceholder={DATE_FORMAT}
     />
   );
 }
