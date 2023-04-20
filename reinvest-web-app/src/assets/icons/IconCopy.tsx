5e833cb6ba830332f49d59f96450eca7707a2160
Reinny Almonte <reinny@hookooekoo.co>
2023-04-20 16:29:48 -0400

    feat: adding icon assets

---
+++ reinvest-web-app/src/assets/icons/IconCopy.tsx
@@ -0,0 +1,24 @@
+import { Icon } from '@hookooekoo/ui-icon';
+
+export const IconCopy = Icon(
+  <svg
+    width="23"
+    height="23"
+    viewBox="0 0 23 23"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M22.1775 19.3747V8.87494V6.25H6.42786V8.87494V19.3747V21.9997H22.1775V19.3747Z"
+      stroke="black"
+      strokeWidth="1.5"
+      strokeLinecap="square"
+    />
+    <path
+      d="M16.9277 6.24988V3.62494V1H14.3027H3.80298H1.17804V3.62494V14.1247V16.7497H3.80298H6.42792"
+      stroke="black"
+      strokeWidth="1.5"
+      strokeLinecap="square"
+    />
+  </svg>,
+);
---
+++ reinvest-web-app/src/assets/icons/IconGift.tsx
@@ -0,0 +1,55 @@
+import { Icon } from '@hookooekoo/ui-icon';
+
+export const IconGift = Icon(
+  <svg
+    width="97"
+    height="96"
+    viewBox="0 0 97 96"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="0.5"
+      width="96"
+      height="96"
+      rx="48"
+      fill="black"
+    />
+    <g clipPath="url(#clip0_3032_307066)">
+      <path
+        d="M62.6667 40.667H33.3333H31.5V46.167V48.0003H60H64.5V46.167V40.667H62.6667Z"
+        stroke="white"
+        strokeWidth="2.75"
+        strokeLinecap="square"
+      />
+      <path
+        d="M48 40.667V64.5003"
+        stroke="white"
+        strokeWidth="2.75"
+        strokeLinecap="square"
+      />
+      <path
+        d="M60.8333 48V60.8333V64.5H57.1667H38.8333H35.1667V60.8333V48"
+        stroke="white"
+        strokeWidth="2.75"
+        strokeLinecap="square"
+      />
+      <path
+        d="M39.75 40.6665C38.5344 40.6665 37.3686 40.1836 36.5091 39.3241C35.6495 38.4645 35.1667 37.2987 35.1667 36.0832C35.1667 34.8676 35.6495 33.7018 36.5091 32.8423C37.3686 31.9827 38.5344 31.4998 39.75 31.4998C41.5186 31.469 43.2517 32.3271 44.7233 33.9623C46.195 35.5974 47.3368 37.9337 48 40.6665C48.6632 37.9337 49.805 35.5974 51.2766 33.9623C52.7483 32.3271 54.4814 31.469 56.25 31.4998C57.4656 31.4998 58.6314 31.9827 59.4909 32.8423C60.3504 33.7018 60.8333 34.8676 60.8333 36.0832C60.8333 37.2987 60.3504 38.4645 59.4909 39.3241C58.6314 40.1836 57.4656 40.6665 56.25 40.6665"
+        stroke="white"
+        strokeWidth="2.75"
+        strokeLinecap="square"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_3032_307066">
+        <rect
+          width="44"
+          height="44"
+          fill="white"
+          transform="translate(26 26)"
+        />
+      </clipPath>
+    </defs>
+  </svg>,
+);
