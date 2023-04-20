490977e98e4b3b1e395f9b5c4c0dafe7cca93c73
Reinny Almonte <reinny@hookooekoo.co>
2023-04-20 16:35:55 -0400

    feat: defining more fixed sizes for avatar component

--- reinvest-web-app/src/components/Avatar.tsx
+++ reinvest-web-app/src/components/Avatar.tsx
@@ -4,18 +4,22 @@ import Image, { ImageProps } from 'next/image';
 
 type PrimitiveProps = Pick<ImageProps, 'src' | 'alt'>;
 interface Props extends PrimitiveProps {
+  fixedSize?: 'sm' | 'md' | 'lg';
   isSizeFixed?: boolean;
 }
 
-export const Avatar = ({ src, alt, isSizeFixed = false }: Props) => {
+export const Avatar = ({ src, alt, isSizeFixed = false, fixedSize = 'lg' }: Props) => {
   const className = cx('relative', {
     'h-28 w-28 lg:h-44 lg:w-44': !isSizeFixed,
-    'h-100 w-100': isSizeFixed,
+    'h-100 w-100': isSizeFixed && fixedSize === 'lg',
+    'h-72 w-72': isSizeFixed && fixedSize === 'md',
+    'h-44 w-44': isSizeFixed && fixedSize === 'sm',
   });
 
   return (
     <PrimitiveAvatar
       className={className}
+      altText={alt}
       image={
         <Image
           className="rounded-full"
