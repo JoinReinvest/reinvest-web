2b4c2837177c27041a17f9eccec3c9c7eed3e314
Reinny Almonte <reinny@hookooekoo.co>
2023-04-23 21:15:44 -0400

    feat: using beneficiary creation flow provider

--- reinvest-web-app/src/layouts/MainLayout.tsx
+++ reinvest-web-app/src/layouts/MainLayout.tsx
@@ -1,14 +1,17 @@
 import { ActiveAccountProvider } from 'providers/ActiveAccountProvider';
 import { PropsWithChildren } from 'react';
+import { BeneficiaryCreationFlowProvider } from 'views/beneficiary-creation';
 
 import { Header } from './components/Header';
 
 export const MainLayout = ({ children }: PropsWithChildren) => {
   return (
     <ActiveAccountProvider>
-      <Header />
+      <BeneficiaryCreationFlowProvider initialStoreFields={{}}>
+        <Header />
 
-      <main className="container mx-auto pb-20 pt-80 lg:pt-100">{children}</main>
+        <main className="container mx-auto pb-20 pt-80 lg:pt-100">{children}</main>
+      </BeneficiaryCreationFlowProvider>
     </ActiveAccountProvider>
   );
 };
