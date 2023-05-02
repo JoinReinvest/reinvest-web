395f758a8439d5bbaae1b40879d5202e4b66e51d
Reinny Almonte <reinny@hookooekoo.co>
2023-05-02 10:38:20 +0200

    feat: enabling to know when the active account is returning from the onboarding flow

--- reinvest-web-app/src/providers/ActiveAccountProvider.tsx
+++ reinvest-web-app/src/providers/ActiveAccountProvider.tsx
@@ -1,17 +1,23 @@
 import { createContext, PropsWithChildren, useContext, useEffect, useMemo, useState } from 'react';
 import { MAXIMUM_NUMBER_OF_BENEFICIARIES } from 'reinvest-app-common/src/constants/account';
-import { useGetIndividualAccount } from 'reinvest-app-common/src/services/queries/getIndividualAccount';
 import { useGetUserProfile } from 'reinvest-app-common/src/services/queries/getProfile';
 import { AccountOverview, Maybe } from 'reinvest-app-common/src/types/graphql';
 import { getApiClient } from 'services/getApiClient';
+import { useSessionStorage } from 'usehooks-ts';
 
 interface ActiveAccountState {
   activeAccount: AccountOverview | null;
   allAccounts: Maybe<AccountOverview>[];
+  arrivesFromOnboarding: boolean;
   /** Accounts that are available to be switched to. */
   availableAccounts: Maybe<AccountOverview>[];
   isAbleToAddBeneficiaries: boolean;
+  setArrivesFromOnboarding: (value: boolean) => void;
   updateActiveAccount: (account: Maybe<AccountOverview>) => void;
+}
+
+enum StorageKeys {
+  HAS_BEEN_ONBOARDED = 'active-account-has-been-onboarded',
 }
 
 const Context = createContext<ActiveAccountState>({
@@ -21,6 +27,9 @@ const Context = createContext<ActiveAccountState>({
   isAbleToAddBeneficiaries: false,
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   updateActiveAccount: () => {},
+  arrivesFromOnboarding: false,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setArrivesFromOnboarding: () => {},
 });
 
 export const useActiveAccount = () => useContext(Context);
@@ -28,23 +37,19 @@ export const useActiveAccount = () => useContext(Context);
 export const ActiveAccountProvider = ({ children }: PropsWithChildren) => {
   const [activeAccount, setActiveAccount] = useState<AccountOverview | null>(null);
   const { data: userProfile } = useGetUserProfile(getApiClient);
-  const { data: individualAccount } = useGetIndividualAccount(getApiClient);
   const allAccounts = useMemo(() => userProfile?.accounts || [], [userProfile]);
   const availableAccounts = useMemo(() => allAccounts.filter(account => account?.id !== activeAccount?.id), [activeAccount, allAccounts]);
+  const [arrivesFromOnboarding, setArrivesFromOnboarding] = useSessionStorage(StorageKeys.HAS_BEEN_ONBOARDED, false);
+
   const isAbleToAddBeneficiaries = 0 < MAXIMUM_NUMBER_OF_BENEFICIARIES;
 
   useEffect(() => {
-    if (individualAccount) {
-      setActiveAccount({
-        id: individualAccount.id,
-        label: individualAccount.label,
-        type: 'INDIVIDUAL',
-        avatar: {
-          ...individualAccount.avatar,
-        },
-      });
+    const firstAccount = userProfile?.accounts?.at(0);
+
+    if (firstAccount) {
+      setActiveAccount(firstAccount);
     }
-  }, [individualAccount]);
+  }, [userProfile]);
 
   const updateActiveAccount = (account: Maybe<AccountOverview>) => {
     if (account) {
@@ -60,6 +65,8 @@ export const ActiveAccountProvider = ({ children }: PropsWithChildren) => {
         updateActiveAccount,
         availableAccounts,
         isAbleToAddBeneficiaries,
+        arrivesFromOnboarding,
+        setArrivesFromOnboarding,
       }}
     >
       {children}
