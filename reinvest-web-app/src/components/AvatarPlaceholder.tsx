95903bbc7cc66885ef22e4bd16efa0f82c26cba1
adammalej <adammalej1@gmail.com>
2023-05-09 10:55:04 +0200

    fix: upgrade avatar image

---
+++ reinvest-web-app/src/components/AvatarPlaceholder.tsx
@@ -0,0 +1,19 @@
+import cx from 'classnames';
+import { DraftAccountType } from 'reinvest-app-common/src/types/graphql';
+
+interface Props {
+  accountType: DraftAccountType;
+  className?: string;
+  label?: string;
+}
+
+export const AvatarPlaceholder = ({ accountType, className = '', label }: Props) => {
+  const styles = cx({
+    'bg-gold': accountType === DraftAccountType.Corporate,
+    'bg-gray-03': accountType === DraftAccountType.Trust,
+    'rounded-full flex justify-center items-center': true,
+    [className]: true,
+  });
+
+  return <div className={styles}>{label}</div>;
+};
