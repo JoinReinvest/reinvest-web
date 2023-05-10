e7b07177b2203f96fe29a90c2907a5780b592c86
Reinny Almonte <reinny@hookooekoo.co>
2023-05-10 08:59:41 +0200

    feat: defining investment view

---
+++ reinvest-web-app/src/views/investment/form-flow/steps/recurring-deposit-schedule.tsx
@@ -0,0 +1,126 @@
+import { IconRecurrent } from 'assets/icons/IconRecurrent';
+import { IconWarning } from 'assets/icons/IconWarning';
+import cx from 'classnames';
+import { Button } from 'components/Button';
+import { ButtonStack } from 'components/FormElements/ButtonStack';
+import { Form } from 'components/FormElements/Form';
+import { FormContent } from 'components/FormElements/FormContent';
+import { ModalTitle } from 'components/ModalElements/Title';
+import { Separator } from 'components/Separator';
+import { Typography } from 'components/Typography';
+import { FormEventHandler } from 'react';
+import {
+  RECURRING_INVESTMENT_INTERVAL_LABELS,
+  RECURRING_INVESTMENT_INTERVALS_WITHIN_MONTH,
+  RecurringInvestmentInterval,
+} from 'reinvest-app-common/src/constants/recurring-investment-intervals';
+import { allRequiredFieldsExists, StepComponentProps, StepParams } from 'reinvest-app-common/src/services/form-flow';
+import { formatDateForRecurrentInvestmentDisplay } from 'reinvest-app-common/src/utilities/dates';
+import { formatInvestmentDateFrequency } from 'reinvest-app-common/src/utilities/dates';
+import { maskCurrency } from 'utils/currency';
+
+import { FlowFields } from '../fields';
+import { Identifiers } from '../identifiers';
+
+const TITLE = 'Recurring Deposit Schedule';
+const MESSAGE_INFORMATION = 'This transaction should take 3-5 business days to complete.';
+
+export const StepRecurringDepositSchedule: StepParams<FlowFields> = {
+  identifier: Identifiers.RECURRING_DEPOSIT_SCHEDULE,
+
+  willBePartOfTheFlow: fields => !!fields._shouldDisplayRecurringInvestment,
+
+  doesMeetConditionFields: fields => {
+    const requiredFields = [
+      fields._selectedAccount,
+      fields.investmentAmount !== undefined,
+      fields._willSetUpRecurringInvestment,
+      fields.recurringInvestmentAmount !== undefined,
+      fields.recurringInvestmentInterval,
+      fields.recurringInvestmentDate,
+    ];
+
+    return allRequiredFieldsExists(requiredFields);
+  },
+
+  Component: ({ storeFields, moveToNextStep }: StepComponentProps<FlowFields>) => {
+    const frequencyLabel = getFrequencyLabel(storeFields.recurringInvestmentInterval, storeFields.recurringInvestmentDate);
+    const investmentAmount = storeFields.recurringInvestmentAmount || '';
+    const investmentAmountForDisplay = maskCurrency(investmentAmount, { addDecimalPoints: true });
+    const startingOnDate = formatDateForRecurrentInvestmentDisplay(storeFields.recurringInvestmentDate || new Date());
+
+    const onSubmit: FormEventHandler<HTMLFormElement> = async event => {
+      event.preventDefault();
+      moveToNextStep();
+    };
+
+    return (
+      <Form onSubmit={onSubmit}>
+        <FormContent willLeaveContentOnTop>
+          <div className="flex w-full justify-center">
+            <IconRecurrent />
+          </div>
+
+          <ModalTitle
+            title={TITLE}
+            isTitleCenteredOnMobile={false}
+          />
+
+          <ul className="flex flex-col gap-16">
+            {generateListItem('From', 'JPMORGAN CHASE BANK, NA ****1234', true)}
+            <Separator />
+            {generateListItem('Frequency', frequencyLabel)}
+            <Separator />
+            {generateListItem('Starting On', startingOnDate)}
+            <Separator />
+            {generateListItem('Amount', investmentAmountForDisplay)}
+          </ul>
+
+          <div className="flex gap-8">
+            <IconWarning className="stroke-gray-01" />
+            <Typography
+              variant="paragraph"
+              className="grow text-gray-01"
+            >
+              {MESSAGE_INFORMATION}
+            </Typography>
+          </div>
+        </FormContent>
+
+        <ButtonStack>
+          <Button
+            type="submit"
+            label="Continue"
+          />
+        </ButtonStack>
+      </Form>
+    );
+  },
+};
+
+function generateListItem(label: string, value: string, uppercase?: boolean) {
+  return (
+    <li className="flex justify-between gap-8">
+      <Typography variant="paragraph-large">{label}</Typography>
+      <Typography
+        variant="paragraph-emphasized"
+        className={cx('text-black-02', { uppercase: !!uppercase })}
+      >
+        {value}
+      </Typography>
+    </li>
+  );
+}
+
+function getFrequencyLabel(frequency: RecurringInvestmentInterval | undefined, investmentDate: Date | undefined) {
+  if (frequency && investmentDate) {
+    const label = RECURRING_INVESTMENT_INTERVAL_LABELS.get(frequency);
+    const date = new Date(investmentDate);
+    const isFrequencyWithinMonth = RECURRING_INVESTMENT_INTERVALS_WITHIN_MONTH.includes(frequency);
+    const dateDisplay = formatInvestmentDateFrequency(date, !isFrequencyWithinMonth);
+
+    return `${label}: ${dateDisplay}`;
+  }
+
+  return '';
+}
---
+++ reinvest-web-app/src/views/investment/form-flow/steps/recurring-investment-amount.tsx
@@ -0,0 +1,87 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from 'components/Button';
+import { ButtonStack } from 'components/FormElements/ButtonStack';
+import { Form } from 'components/FormElements/Form';
+import { FormContent } from 'components/FormElements/FormContent';
+import { FormMessage } from 'components/FormElements/FormMessage';
+import { InvestmentCard } from 'components/FormElements/InvestmentCard';
+import { ModalTitle } from 'components/ModalElements/Title';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { allRequiredFieldsExists, StepComponentProps, StepParams } from 'reinvest-app-common/src/services/form-flow';
+import { maskCurrency } from 'utils/currency';
+import { Schema, z } from 'zod';
+
+import { FlowFields } from '../fields';
+import { Identifiers } from '../identifiers';
+
+const TITLE = 'Choose the amount for your recurring investment';
+const MINIMUM_AMOUNT = 100;
+const MASKED_MINIMUM_AMOUNT = maskCurrency(MINIMUM_AMOUNT);
+
+type Fields = Pick<FlowFields, 'recurringInvestmentAmount'>;
+
+const schema: Schema<Fields> = z.object({
+  recurringInvestmentAmount: z.number().min(MINIMUM_AMOUNT, `Minimum investment amount is ${MASKED_MINIMUM_AMOUNT}`),
+});
+
+export const StepRecurringInvestmentAmount: StepParams<FlowFields> = {
+  identifier: Identifiers.RECURRING_INVESTMENT_AMOUNT,
+
+  willBePartOfTheFlow: fields => !!fields._shouldDisplayRecurringInvestment,
+
+  doesMeetConditionFields: fields => {
+    const requiredFields = [fields._selectedAccount, fields.investmentAmount !== undefined, fields._willSetUpRecurringInvestment];
+
+    return allRequiredFieldsExists(requiredFields);
+  },
+
+  Component: ({ storeFields, updateStoreFields, moveToNextStep }: StepComponentProps<FlowFields>) => {
+    const defaultValues: Fields = { recurringInvestmentAmount: storeFields.recurringInvestmentAmount };
+    const { handleSubmit, setValue, formState } = useForm<Fields>({
+      mode: 'onChange',
+      defaultValues: async () => defaultValues,
+      resolver: zodResolver(schema),
+    });
+
+    const shouldButtonBeDisabled = !formState.isValid || formState.isSubmitting;
+    const errorMessage = formState.errors.recurringInvestmentAmount?.message;
+
+    const onSubmit: SubmitHandler<Fields> = async ({ recurringInvestmentAmount }) => {
+      await updateStoreFields({ recurringInvestmentAmount, recurringInvestmentDate: new Date() });
+
+      moveToNextStep();
+    };
+
+    return (
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormContent willLeaveContentOnTop>
+          <ModalTitle
+            title={TITLE}
+            isTitleCenteredOnMobile={false}
+          />
+
+          <InvestmentCard
+            defaultValue={defaultValues.recurringInvestmentAmount}
+            onChange={value => setValue('recurringInvestmentAmount', value, { shouldValidate: true })}
+            currentBankAccount="Checking **** **** **** 0000"
+            onChangeBankAccount={() => {
+              // eslint-disable-next-line no-console
+              console.info('change bank account');
+            }}
+            className="mx-auto"
+          />
+
+          {!!errorMessage && <FormMessage message={errorMessage} />}
+        </FormContent>
+
+        <ButtonStack>
+          <Button
+            type="submit"
+            label="Continue"
+            disabled={shouldButtonBeDisabled}
+          />
+        </ButtonStack>
+      </Form>
+    );
+  },
+};
