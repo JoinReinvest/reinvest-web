eb46e0bd5f405f077ccaedfe1156a9275e9344f9
AdamHKEK <126069460+AdamHKEK@users.noreply.github.com>
2023-05-11 12:03:45 +0200

    Connection with plaid (#319)
    * feat: connectcion with plaid
    
    * feat: open bank account selection on button click
    
    * feat: upgrade yarn lock
    
    * feat: upgrade yarn lock
    
    * feat: upgrade height of iframe and add loading spinner

--- reinvest-web-app/src/views/bank-account/form-flow/steps/landing.tsx
+++ reinvest-web-app/src/views/bank-account/form-flow/steps/landing.tsx
@@ -9,10 +9,14 @@ import { Form } from 'components/FormElements/Form';
 import { FormContent } from 'components/FormElements/FormContent';
 import { ModalTitle } from 'components/ModalElements/Title';
 import { Typography } from 'components/Typography';
-import { FormEventHandler } from 'react';
+import { FormEventHandler, useEffect } from 'react';
 import { StepComponentProps, StepParams } from 'reinvest-app-common/src/services/form-flow';
+import { useReadBankAccount } from 'reinvest-app-common/src/services/queries/readBankAccount';
 import { lowerCaseWithoutSpacesGenerator } from 'utils/optionValueGenerators';
 
+import { IconSpinner } from '../../../../assets/icons/IconSpinner';
+import { useActiveAccount } from '../../../../providers/ActiveAccountProvider';
+import { getApiClient } from '../../../../services/getApiClient';
 import { FlowFields } from '../fields';
 import { Identifiers } from '../identifiers';
 
@@ -34,58 +38,98 @@ const LIST_ITEMS = [
 export const StepLanding: StepParams<FlowFields> = {
   identifier: Identifiers.LANDING,
 
-  isAValidationView: true,
+  Component: ({ moveToNextStep, updateStoreFields }: StepComponentProps<FlowFields>) => {
+    const { activeAccount } = useActiveAccount();
+    const {
+      refetch,
+      isError,
+      data: readBankAccountData,
+      isSuccess: isReadBankAccountSuccess,
+      isLoading: isReadBankAccountLoading,
+    } = useReadBankAccount(getApiClient, {
+      accountId: activeAccount?.id || '',
+      config: {
+        enabled: false,
+        retry: false,
+      },
+    });
 
-  Component: ({ moveToNextStep }: StepComponentProps<FlowFields>) => {
     const onSubmit: FormEventHandler<HTMLFormElement> = async event => {
       event.preventDefault();
 
       moveToNextStep();
     };
 
+    useEffect(() => {
+      refetch();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+      if (isReadBankAccountSuccess && readBankAccountData?.accountNumber) {
+        updateStoreFields({ bankAccount: readBankAccountData.accountNumber });
+        moveToNextStep();
+      }
+    }, [isReadBankAccountSuccess, moveToNextStep, updateStoreFields, readBankAccountData?.accountNumber]);
+
+    useEffect(() => {
+      if (isError) {
+        updateStoreFields({ bankAccount: '' });
+      }
+    }, [isError, moveToNextStep, updateStoreFields]);
+
     return (
       <Form onSubmit={onSubmit}>
-        <FormContent>
-          <div className="flex flex-col gap-48">
-            <header className="flex justify-center gap-14">
-              <LogoIcon2 className="h-25 w-auto" />
-
-              <Separator
-                orientation="vertical"
-                className="w-1 bg-black-01"
-              />
-
-              <LogoPlaid />
-            </header>
-
-            <div className="flex flex-col gap-40">
-              <ModalTitle title={TITLE} />
-
-              <ul className="flex flex-col gap-24">
-                {LIST_ITEMS.map(({ icon, title, description }) => (
-                  <li
-                    className="flex gap-8"
-                    key={lowerCaseWithoutSpacesGenerator(title)}
-                  >
-                    {icon}
-
-                    <div className="flex flex-col gap-8">
-                      <Typography variant="paragraph-emphasized">{title}</Typography>
-                      <Typography variant="paragraph-large">{description}</Typography>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
+        {isReadBankAccountLoading && (
+          <div className="flex h-full flex-col items-center gap-32 lg:justify-center">
+            <IconSpinner />
           </div>
-        </FormContent>
+        )}
+        {!isReadBankAccountLoading && (
+          <>
+            <FormContent>
+              <div className="flex flex-col gap-48">
+                <header className="flex justify-center gap-14">
+                  <LogoIcon2 className="h-25 w-auto" />
 
-        <ButtonStack>
-          <Button
-            type="submit"
-            label={BUTTON_LABEL}
-          />
-        </ButtonStack>
+                  <Separator
+                    orientation="vertical"
+                    className="w-1 bg-black-01"
+                  />
+
+                  <LogoPlaid />
+                </header>
+
+                <div className="flex flex-col gap-40">
+                  <ModalTitle title={TITLE} />
+
+                  <ul className="flex flex-col gap-24">
+                    {LIST_ITEMS.map(({ icon, title, description }) => (
+                      <li
+                        className="flex gap-8"
+                        key={lowerCaseWithoutSpacesGenerator(title)}
+                      >
+                        {icon}
+
+                        <div className="flex flex-col gap-8">
+                          <Typography variant="paragraph-emphasized">{title}</Typography>
+                          <Typography variant="paragraph-large">{description}</Typography>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </FormContent>
+
+            <ButtonStack>
+              <Button
+                type="submit"
+                label={BUTTON_LABEL}
+              />
+            </ButtonStack>
+          </>
+        )}
       </Form>
     );
   },
--- reinvest-web-app/src/views/dashboard/index.tsx
+++ reinvest-web-app/src/views/dashboard/index.tsx
@@ -8,10 +8,11 @@ import { useToggler } from 'hooks/toggler';
 import { renderBlogCard } from 'pages/education';
 import { useActiveAccount } from 'providers/ActiveAccountProvider';
 import { useEffect } from 'react';
-import { InitialInvestmentView } from 'views/initial-investment';
 import { InitialInvestmentFormFlowProvider } from 'views/initial-investment/form-flow';
-import { InvestmentView } from 'views/investment';
 import { InvestmentFlowProvider } from 'views/investment/form-flow';
+
+import { BankAccountFlow } from '../bank-account';
+import { BankAccountFlowProvider } from '../bank-account/form-flow';
 
 interface Props {
   arePostsReady: boolean;
@@ -19,10 +20,9 @@ interface Props {
 }
 
 export const DashboardView = ({ posts, arePostsReady }: Props) => {
-  const { activeAccount, arrivesFromOnboarding, setArrivesFromOnboarding } = useActiveAccount();
+  const { arrivesFromOnboarding, setArrivesFromOnboarding } = useActiveAccount();
   // Display the initial investment flow if the user arrives from the onboarding flow
   const [displayInitialInvestmentFlow, toggleDisplayInitialInvestmentFlow] = useToggler(arrivesFromOnboarding);
-  const [isInvestmentFlowOpen, toggleIsInvestmentFlowOpen] = useToggler(false);
 
   useEffect(() => {
     setArrivesFromOnboarding(false);
@@ -30,56 +30,52 @@ export const DashboardView = ({ posts, arePostsReady }: Props) => {
   }, []);
 
   return (
-    <InitialInvestmentFormFlowProvider initialStoreFields={{}}>
-      <InvestmentFlowProvider initialStoreFields={{}}>
-        <div className="flex flex-col gap-16 lg:flex-row lg:gap-24">
-          <div className="lg:flex lg:w-full lg:max-w-330 lg:flex-col lg:justify-between lg:gap-8">
-            <Typography
-              variant="h3"
-              className="hidden lg:block"
-            >
-              Dashboard
-            </Typography>
-            <Typography variant="h2">$0.00</Typography>
-            <Typography
-              variant="paragraph-emphasized"
-              className="mt-8 text-gray-02 lg:mt-0"
-            >
-              Account Value
-            </Typography>
-            <PositionTotal className="hidden lg:mt-8 lg:block" />
-            <NetReturns className="hidden lg:mt-8 lg:block" />
+    <BankAccountFlowProvider initialStoreFields={{ _hasCompletedFlow: false, bankAccount: '' }}>
+      <InitialInvestmentFormFlowProvider initialStoreFields={{}}>
+        <InvestmentFlowProvider initialStoreFields={{}}>
+          <div className="flex flex-col gap-16 lg:flex-row lg:gap-24">
+            <div className="lg:flex lg:w-full lg:max-w-330 lg:flex-col lg:justify-between lg:gap-8">
+              <Typography
+                variant="h3"
+                className="hidden lg:block"
+              >
+                Dashboard
+              </Typography>
+              <Typography variant="h2">$0.00</Typography>
+              <Typography
+                variant="paragraph-emphasized"
+                className="mt-8 text-gray-02 lg:mt-0"
+              >
+                Account Value
+              </Typography>
+              <PositionTotal className="hidden lg:mt-8 lg:block" />
+              <NetReturns className="hidden lg:mt-8 lg:block" />
+              <Button
+                label="Invest"
+                onClick={() => toggleDisplayInitialInvestmentFlow()}
+                className="hidden lg:mt-16 lg:block"
+              />
+            </div>
+            <div className="flex h-180 flex-col gap-8 border border-gray-04 lg:h-auto lg:w-full lg:gap-24">
+              <AreaChart />
+            </div>
+
             <Button
               label="Invest"
-              onClick={() => toggleIsInvestmentFlowOpen()}
-              className="hidden lg:mt-16 lg:block"
+              className="lg:hidden"
+              onClick={() => toggleDisplayInitialInvestmentFlow()}
             />
+            <PositionTotal className="lg:hidden" />
+            <NetReturns className="lg:hidden" />
           </div>
-          <div className="flex h-180 flex-col gap-8 border border-gray-04 lg:h-auto lg:w-full lg:gap-24">
-            <AreaChart />
-          </div>
+          {arePostsReady && <div className="mt-16 flex flex-col gap-16 lg:mt-24 lg:grid lg:grid-cols-3 lg:gap-y-36">{posts.map(renderBlogCard)}</div>}
 
-          <Button
-            label="Invest"
-            className="lg:hidden"
-            onClick={() => toggleIsInvestmentFlowOpen()}
+          <BankAccountFlow
+            isOpen={displayInitialInvestmentFlow}
+            toggleIsOpen={toggleDisplayInitialInvestmentFlow}
           />
-          <PositionTotal className="lg:hidden" />
-          <NetReturns className="lg:hidden" />
-        </div>
-        {arePostsReady && <div className="mt-16 flex flex-col gap-16 lg:mt-24 lg:grid lg:grid-cols-3 lg:gap-y-36">{posts.map(renderBlogCard)}</div>}
-
-        <InitialInvestmentView
-          isOpen={displayInitialInvestmentFlow}
-          toggleIsOpen={toggleDisplayInitialInvestmentFlow}
-        />
-
-        <InvestmentView
-          isOpen={isInvestmentFlowOpen}
-          toggleIsOpen={toggleIsInvestmentFlowOpen}
-          activeAccount={activeAccount}
-        />
-      </InvestmentFlowProvider>
-    </InitialInvestmentFormFlowProvider>
+        </InvestmentFlowProvider>
+      </InitialInvestmentFormFlowProvider>
+    </BankAccountFlowProvider>
   );
 };
