16187186a1f79f01857136c0b20ad5c303ce7eff
Reinny Almonte <reinny@hookooekoo.co>
2023-05-12 11:09:59 +0200

    feat: passing "disabled" prop to button-back component

--- reinvest-web-app/src/components/ButtonBack.tsx
+++ reinvest-web-app/src/components/ButtonBack.tsx
@@ -2,15 +2,16 @@ import { IconArrowLeft } from 'assets/icons/IconArrowLeft';
 import { Typography } from 'components/Typography';
 import { ButtonHTMLAttributes, forwardRef } from 'react';
 
-type PrimitiveProps = Pick<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'>;
+type PrimitiveProps = Pick<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick' | 'disabled'>;
 type Props = PrimitiveProps;
 
-export const ButtonBack = forwardRef<HTMLButtonElement, Props>(({ onClick }, ref) => (
+export const ButtonBack = forwardRef<HTMLButtonElement, Props>(({ onClick, disabled }, ref) => (
   <button
     type="button"
     className="flex items-center"
     onClick={onClick}
     ref={ref}
+    disabled={disabled}
   >
     <IconArrowLeft className="stroke-black-01" />
 
