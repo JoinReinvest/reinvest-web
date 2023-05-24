1beae2093add0b5e495d0329e3d52f521bbbb2dd
AdamHKEK <adam@hookooekoo.co>
2023-05-24 15:34:27 +0200

    fix: remove x icon

--- reinvest-web-app/src/components/ModalBlackFullscreen/Header.tsx
+++ reinvest-web-app/src/components/ModalBlackFullscreen/Header.tsx
@@ -2,7 +2,7 @@ import { DialogClose } from '@hookooekoo/ui-dialog';
 import { IconArrowLeft } from 'assets/icons/IconArrowLeft';
 import { Logo } from 'assets/Logo';
 
-import { IconX } from '../../assets/icons/IconX';
+import { IconClose } from '../../assets/icons/IconClose';
 
 interface Props {
   isBackButtonEnabled?: boolean;
@@ -13,7 +13,7 @@ export const Header = ({ isBackButtonEnabled }: Props) => {
 
   return (
     <header className="black-modal-header relative flex h-32 w-full items-center justify-center px-20 lg:h-48">
-      <DialogClose>{isBackButtonEnabled ? <IconArrowLeft className={iconStyles} /> : <IconX className={iconStyles} />}</DialogClose>
+      <DialogClose>{isBackButtonEnabled ? <IconArrowLeft className={iconStyles} /> : <IconClose className={iconStyles} />}</DialogClose>
 
       <Logo className="fill-white" />
     </header>
