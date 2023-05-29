d940bd7ce94884a94ff6d25bc03643635ac9c83c
Reinny Almonte <reinny@hookooekoo.co>
2023-05-29 13:45:52 +0200

    feat: retaining active account identifier on the session storage

---
+++ reinvest-web-app/src/constants/storage-keys.ts
@@ -0,0 +1,4 @@
+export enum StorageKeys {
+  LATEST_ACCOUNT_ONBOARDED = 'latest-account-onboarded',
+  ACTIVE_ACCOUNT_IDENTIFIER = 'active-account-identifier',
+}
--- reinvest-web-app/src/providers/ActiveAccountProvider/hooks/profile-account.ts
+++ reinvest-web-app/src/providers/ActiveAccountProvider/hooks/profile-account.ts
@@ -1,9 +1,11 @@
+import { StorageKeys } from 'constants/storage-keys';
 import { useStaticState } from 'hooks/static-state';
 import { useEffect, useMemo, useState } from 'react';
 import { useGetUserProfile } from 'reinvest-app-common/src/services/queries/getProfile';
 import { AccountOverview, AccountType, Maybe, Profile } from 'reinvest-app-common/src/types/graphql';
 import { getApiClient } from 'services/getApiClient';
 import { QueryMeta } from 'types/queries';
+import { useSessionStorage } from 'usehooks-ts';
 
 interface Return {
   activeAccount: AccountOverview | null;
@@ -17,27 +19,36 @@ interface Return {
 export function useProfileAccounts(): Return {
   const { data: userProfile, ...userProfileMeta } = useGetUserProfile(getApiClient);
   const [activeAccount, setActiveAccount] = useState<AccountOverview | null>(null);
+  const [activeAccountId, setActiveAccountId] = useSessionStorage<string | null>(StorageKeys.ACTIVE_ACCOUNT_IDENTIFIER, null);
   const [previousAccount, setPreviousAccount] = useStaticState<AccountOverview | null>(null);
   const allAccounts = useMemo(() => userProfile?.accounts || [], [userProfile]);
 
   useEffect(() => {
     function initializeActiveAccount() {
-      const nonBeneficiaryAccounts = userProfile?.accounts?.filter(account => account?.type !== AccountType.Beneficiary);
+      if (activeAccountId) {
+        const storedAccount = allAccounts.find(account => account?.id === activeAccountId);
+        storedAccount && setActiveAccount(storedAccount);
+      } else {
+        const nonBeneficiaryAccounts = userProfile?.accounts?.filter(account => account?.type !== AccountType.Beneficiary);
 
-      if (nonBeneficiaryAccounts?.length) {
-        const firstAccount = nonBeneficiaryAccounts.at(0);
+        if (nonBeneficiaryAccounts?.length) {
+          const firstAccount = nonBeneficiaryAccounts.at(0);
 
-        setActiveAccount(firstAccount || null);
+          setActiveAccount(firstAccount || null);
+          setActiveAccountId(firstAccount?.id || null);
+        }
       }
     }
 
     initializeActiveAccount();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userProfile]);
 
   const updateActiveAccount = (account: Maybe<AccountOverview>) => {
     if (account) {
       setPreviousAccount(activeAccount);
       setActiveAccount(account);
+      setActiveAccountId(account?.id ?? null);
     }
   };
 
