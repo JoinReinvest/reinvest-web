9f0ca92fcff6b2033fa2bbd74f906b2114876838
Reinny Almonte <126076121+reinny-hkek@users.noreply.github.com>
2023-06-09 07:28:01 +0200

    Create update addres flow (#486)
    * style: applying proper styles to select components under white modals
    
    * feat: defining flow for updating the address of the profile

---
+++ reinvest-web-app/src/views/account-management/flows/update-address/flow.tsx
@@ -0,0 +1,37 @@
+import { useActiveAccount } from 'providers/ActiveAccountProvider';
+import { useEffect } from 'react';
+import { createFormFlow } from 'reinvest-app-common/src/services/form-flow';
+
+import { FlowFields } from './interfaces';
+import { STEPS } from './steps';
+
+const [useFlow, FlowProvider] = createFormFlow<FlowFields>({ steps: STEPS });
+
+function InnerFlow() {
+  const { userProfile, userProfileMeta } = useActiveAccount();
+  const { CurrentStepView, updateStoreFields } = useFlow();
+
+  const hasLoadedUserProfile = userProfileMeta?.isSuccess;
+
+  useEffect(() => {
+    async function initializeCurrentAddress() {
+      const address = userProfile?.details?.address;
+
+      if (hasLoadedUserProfile && address) {
+        await updateStoreFields({ _currentAddress: address });
+      }
+    }
+
+    initializeCurrentAddress();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hasLoadedUserProfile]);
+
+  return <CurrentStepView />;
+}
+
+export const FlowUpdateAddress = () => (
+  <FlowProvider initialStoreFields={{}}>
+    <InnerFlow />
+  </FlowProvider>
+);
