b69a3e6ae7d3cb69700ed0aded7c4de9bbc1ce23
Reinny Almonte <126076121+reinny-hkek@users.noreply.github.com>
2023-06-16 08:05:51 +0200

    Connect Investment History with API + Connect "cancel pending transaction flow" with API (#501)
    * refactor: abstracting notification-item-observer to intersection-observer hook
    
    * chore: updating dependencies with latest state of common repository
    
    * feat: partially using the API to retrieve the investment history
    
    * refactor: migrating queries to common repository

--- reinvest-web-app/src/views/account-management/flows/investment-history/components/InvestmentDetail.tsx
+++ reinvest-web-app/src/views/account-management/flows/investment-history/components/InvestmentDetail.tsx
@@ -5,26 +5,24 @@ interface Props {
   value: string;
 }
 
-export function InvestmentDetail({ title, value }: Props) {
-  return (
-    <div className="flex flex-col">
-      <dt className="capitalize">
-        <Typography
-          variant="paragraph"
-          className="text-gray-01"
-        >
-          {title}
-        </Typography>
-      </dt>
+export const InvestmentDetail = ({ title, value }: Props) => (
+  <div className="flex flex-col">
+    <dt className="capitalize">
+      <Typography
+        variant="paragraph"
+        className="text-gray-01"
+      >
+        {title}
+      </Typography>
+    </dt>
 
-      <dd>
-        <Typography
-          variant="h6"
-          className="capitalize"
-        >
-          {value}
-        </Typography>
-      </dd>
-    </div>
-  );
-}
+    <dd>
+      <Typography
+        variant="h6"
+        className="capitalize"
+      >
+        {value}
+      </Typography>
+    </dd>
+  </div>
+);
--- reinvest-web-app/src/views/account-management/flows/investment-history/interfaces.ts
+++ reinvest-web-app/src/views/account-management/flows/investment-history/interfaces.ts
@@ -1,20 +1,11 @@
-import { Scalars, Usd } from 'reinvest-app-common/src/types/graphql';
+import { InvestmentOverview, Maybe } from 'reinvest-app-common/src/types/graphql';
 
 export interface FlowFields {
   _cancelledInvestmentDetails?: {
     amountFormatted: string;
     tradeId: string;
   };
-  _investments?: InvestmentOverview[];
+  _investments?: Maybe<InvestmentOverview>[];
   _selectedInvesmentId?: string;
   _willCancelInvestment?: boolean;
-}
-
-// TO-DO: Deprecate once GraphQL types are generated
-// with the API state from the DEV environment.
-export interface InvestmentOverview {
-  amount: Usd;
-  createdAt: Scalars['ISODateTime'];
-  id: Scalars['ID'];
-  tradeId: Scalars['String'];
 }
