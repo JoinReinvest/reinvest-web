2846ced86affc1da30eec6aa222cfc531ad3a976
Reinny Almonte <reinny@hookooekoo.co>
2023-06-21 12:15:33 +0200

    feat: defining account management flow for updating bank account

---
+++ reinvest-web-app/src/views/account-management/flows/bank-account/steps/index.ts
@@ -0,0 +1,6 @@
+import { StepBankAccountSelection } from './bank-account-selection';
+import { StepConfirmation } from './confirmation';
+import { StepCurrentBankAccount } from './current-bank-account';
+import { StepDisclaimer } from './disclaimer';
+
+export const STEPS = [StepCurrentBankAccount, StepDisclaimer, StepBankAccountSelection, StepConfirmation];
