cee598d5d784a0793cec62514639570ed5c6cc31
Reinny Almonte <reinny@hookooekoo.co>
2023-06-27 11:54:08 +0200

    fix: closing the funds withdrawal flow if the account cannot withdraw any funds

--- reinvest-web-app/src/views/account-management/flows/funds-withdrawal/steps/loading.tsx
+++ reinvest-web-app/src/views/account-management/flows/funds-withdrawal/steps/loading.tsx
@@ -2,6 +2,7 @@ import { IconSpinner } from 'assets/icons/IconSpinner';
 import { useEffect } from 'react';
 import { StepComponentProps, StepParams } from 'reinvest-app-common/src/services/form-flow';
 import { AgreementStatus, FundsWithdrawalRequestStatus } from 'reinvest-app-common/src/types/graphql';
+import { useFlowsManager } from 'views/account-management/contexts/FlowsManager';
 
 import { FlowFields, FlowStepIdentifiers } from '../interfaces';
 import { useFundsWithdrawalManager } from '../providers/FundsWithdrawal';
@@ -12,43 +13,44 @@ export const StepLoading: StepParams<FlowFields> = {
   isAValidationView: true,
 
   Component: ({ updateStoreFields, moveToNextStep, moveToStepByIdentifier }: StepComponentProps<FlowFields>) => {
+    const { setCurrentFlowIdentifier } = useFlowsManager();
     const { simulation, simulationMeta, fundsWithdrawalRequest, currentRequestMeta, subscriptionAgreement, currentAgreementMeta } = useFundsWithdrawalManager();
 
     useEffect(() => {
       async function moveToStepBasedOnState() {
-        await updateStoreFields({ _canWithdrawFunds: !!simulation?.canWithdraw });
+        if (simulationMeta?.isSuccess && !currentRequestMeta?.isLoading && !currentAgreementMeta?.isLoading) {
+          await updateStoreFields({ _canWithdrawFunds: !!simulation?.canWithdraw });
 
-        if (!simulation?.canWithdraw) {
-          // TO-DO: View for cannot wihdraw funds message
+          if (!simulation?.canWithdraw) {
+            // TO-DO: View for cannot wihdraw funds message
+            setCurrentFlowIdentifier(null);
+          }
+
+          if (subscriptionAgreement?.status === AgreementStatus.WaitingForSignature) {
+            moveToStepByIdentifier(FlowStepIdentifiers.SUBSCRIPTION_AGREEMENT);
+
+            return;
+          }
+
+          if (fundsWithdrawalRequest?.status === FundsWithdrawalRequestStatus.Draft) {
+            moveToStepByIdentifier(FlowStepIdentifiers.SUBSCRIPTION_AGREEMENT);
+
+            return;
+          }
+
+          if (fundsWithdrawalRequest?.status === FundsWithdrawalRequestStatus.AwaitingDecision) {
+            moveToStepByIdentifier(FlowStepIdentifiers.CONFIRMATION);
+
+            return;
+          }
+
+          moveToNextStep();
         }
-
-        if (subscriptionAgreement?.status === AgreementStatus.WaitingForSignature) {
-          moveToStepByIdentifier(FlowStepIdentifiers.SUBSCRIPTION_AGREEMENT);
-
-          return;
-        }
-
-        if (fundsWithdrawalRequest?.status === FundsWithdrawalRequestStatus.Draft) {
-          moveToStepByIdentifier(FlowStepIdentifiers.SUBSCRIPTION_AGREEMENT);
-
-          return;
-        }
-
-        if (fundsWithdrawalRequest?.status === FundsWithdrawalRequestStatus.AwaitingDecision) {
-          moveToStepByIdentifier(FlowStepIdentifiers.CONFIRMATION);
-
-          return;
-        }
-
-        moveToNextStep();
       }
 
-      if (simulationMeta?.isSuccess && currentRequestMeta?.isSuccess && currentAgreementMeta?.isSuccess) {
-        moveToStepBasedOnState();
-      }
-
+      moveToStepBasedOnState();
       // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [simulationMeta?.isSuccess, currentRequestMeta?.isSuccess, currentAgreementMeta?.isSuccess, fundsWithdrawalRequest?.status]);
+    }, [simulationMeta?.isSuccess, currentRequestMeta?.isLoading, currentAgreementMeta?.isLoading, fundsWithdrawalRequest?.status]);
 
     return (
       <div className="flex h-full flex-col items-center gap-32 lg:justify-center">
