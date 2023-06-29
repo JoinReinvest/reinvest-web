0710d3ea00135eb1e5e5f2cc53804129b56c7862
Reinny Almonte <reinny@hookooekoo.co>
2023-06-29 09:36:57 +0200

    feat: notification flow fow recurring investment failed

--- reinvest-web-app/src/providers/AccountManagement/constants.ts
+++ reinvest-web-app/src/providers/AccountManagement/constants.ts
@@ -6,4 +6,5 @@ export const QUERY_MANAGED_FLOWS = new Map<QueryFlowIdentifiers, FlowIdentifiers
   [QueryFlowIdentifiers.MANAGE_BANK_ACCOUNT, FlowIdentifiers.BANK_ACCOUNT],
   [QueryFlowIdentifiers.MANAGE_PASSWORD, FlowIdentifiers.CHANGE_PASSWORD],
   [QueryFlowIdentifiers.MANAGE_INVESTMENT, FlowIdentifiers.INVESTMENT_HISTORY],
+  [QueryFlowIdentifiers.MANAGE_RECURRING_INVESTMENT, FlowIdentifiers.RECURRING_INVESTMENTS],
 ]);
