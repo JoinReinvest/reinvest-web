7917d4890f764494a97fe287ac00e0e6c518cdf4
AdamHKEK <adam@hookooekoo.co>
2023-06-30 09:40:34 +0200

    feat: create change compliance flow

---
+++ reinvest-web-app/src/providers/AccountManagement/flows/edit-compliance/steps/index.tsx
@@ -0,0 +1,8 @@
+import { StepCompanyTickerSymbols } from './company-ticker-symbols';
+import { StepConfirmation } from './confirmation';
+import { StepCurrentCompliance } from './current-compliance';
+import { StepFinraInstitution } from './finra-institution';
+import { StepSelectCompliance } from './select-compliance';
+import { StepSeniorPoliticalFigure } from './senior-political-figure';
+
+export const STEPS = [StepCurrentCompliance, StepSelectCompliance, StepFinraInstitution, StepCompanyTickerSymbols, StepSeniorPoliticalFigure, StepConfirmation];
