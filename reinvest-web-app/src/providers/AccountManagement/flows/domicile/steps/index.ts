71454fb4a6f0d7fc9ec5e2f8b91673733207b7d6
Reinny Almonte <reinny@hookooekoo.co>
2023-07-03 10:29:20 +0200

    feat: displaying current domicile prior to updating it

--- reinvest-web-app/src/providers/AccountManagement/flows/domicile/steps/index.ts
+++ reinvest-web-app/src/providers/AccountManagement/flows/domicile/steps/index.ts
@@ -1,7 +1,8 @@
 import { StepConfirmation } from './confirmation';
+import { StepCurrentDomicile } from './current-domicile';
 import { StepDomicileGreenCard } from './domicile-green-card';
 import { StepDomicileType } from './domicile-type';
 import { StepDomicileVisa } from './domicile-visa';
 import { StepLoading } from './loading';
 
-export const STEPS = [StepLoading, StepDomicileType, StepDomicileGreenCard, StepDomicileVisa, StepConfirmation];
+export const STEPS = [StepLoading, StepCurrentDomicile, StepDomicileType, StepDomicileGreenCard, StepDomicileVisa, StepConfirmation];
