00e8a2cd4cf435975a0c042dca04ccf06231fe59
Reinny Almonte <reinny@hookooekoo.co>
2023-07-04 10:19:43 +0200

    refactor: abstracting compliance statements

---
+++ reinvest-web-app/src/constants/compliances.ts
@@ -0,0 +1,9 @@
+export const FINRA_STATEMENT = `Are you or anyone in your immediate household, or, for any non-natural person, any officers, directors, or any person that owns or controls 5% (or
+  greater) of the equity, associated with a FINRA member, organization, or the SEC.`;
+
+export const COMPANY_STATEMENT = `Are you or anyone in your household or immediate family, or, for any non-natural person, any of its directors, trustees, 10% (or more) equity
+  holder, an officer, or member of the board of directors of a publicly traded company?`;
+
+export const POLITICAL_STATEMENT = `Are you or any of your immediate family a senior political figure?`;
+
+export const NONE_STATEMENT = 'None of the above apply';
