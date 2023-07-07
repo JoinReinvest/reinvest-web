388d8c6846fddecb192ddb9d95a3d495eda26a89
AdamHKEK <adam@hookooekoo.co>
2023-07-07 12:00:39 +0200

    fix: initials or image in manage account on mobile view

--- reinvest-web-app/src/components/ModalWhite/HeaderMobile.tsx
+++ reinvest-web-app/src/components/ModalWhite/HeaderMobile.tsx
@@ -1,9 +1,9 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { IconClose } from 'assets/icons/IconClose';
-import placeholderImage from 'assets/images/profile-picture-placeholder.png';
 import { Logo } from 'assets/Logo';
 import { Avatar } from 'components/Avatar';
 import { useActiveAccount } from 'providers/ActiveAccountProvider';
+import { AccountType } from 'reinvest-app-common/src/types/graphql';
 
 interface Props {
   hideLogoOnMobile?: boolean;
@@ -11,6 +11,7 @@ interface Props {
 
 export function HeaderMobile({ hideLogoOnMobile = false }: Props) {
   const { activeAccount } = useActiveAccount();
+  const avatarInitials = activeAccount?.avatar?.initials;
 
   return (
     <header className="flex items-center justify-between px-24 pb-22 pt-12 text-black-01 md:hidden md:px-44">
@@ -21,8 +22,10 @@ export function HeaderMobile({ hideLogoOnMobile = false }: Props) {
       {!hideLogoOnMobile && <Logo className="fill-black-01" />}
 
       <Avatar
-        src={activeAccount?.avatar?.url || placeholderImage}
-        alt={activeAccount?.avatar?.initials || ''}
+        src={activeAccount?.avatar?.url ?? undefined}
+        label={avatarInitials ?? undefined}
+        alt={avatarInitials ?? `${activeAccount?.label}'s profile picture`}
+        accountType={activeAccount?.type || AccountType.Individual}
         isSizeFixed
         fixedSize="xs"
       />
