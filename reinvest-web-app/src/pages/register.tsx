106feb3f729c4bab03caee535c4162818cb3fa44
Reinny Almonte <reinny@hookooekoo.co>
2023-07-10 09:43:29 +0200

    test: disabling referral code

--- reinvest-web-app/src/pages/register.tsx
+++ reinvest-web-app/src/pages/register.tsx
@@ -1,24 +1,24 @@
-import { GetServerSideProps } from 'next';
-import { ParsedUrlQuery } from 'querystring';
+// import { GetServerSideProps } from 'next';
+// import { ParsedUrlQuery } from 'querystring';
 import { RegistrationView } from 'views/register';
 import { RegisterFormFlowProvider } from 'views/register/form-flow';
 
-interface Props {
-  referralCode?: string;
-}
+// interface Props {
+//   referralCode?: string;
+// }
 
-interface Params extends ParsedUrlQuery, Props {}
+// interface Params extends ParsedUrlQuery, Props {}
 
-export default function PageRegister({ referralCode }: Props) {
+export default function PageRegister() {
   return (
-    <RegisterFormFlowProvider initialStoreFields={{ email: '', referralCode: referralCode, password: '', authenticationCode: '' }}>
+    <RegisterFormFlowProvider initialStoreFields={{ email: '', referralCode: '', password: '', authenticationCode: '' }}>
       <RegistrationView />
     </RegisterFormFlowProvider>
   );
 }
 
-export const getServerSideProps: GetServerSideProps<Props, Params> = async context => {
-  const referralCode = context.query.referral?.toString();
+// export const getServerSideProps: GetServerSideProps<Props, Params> = async context => {
+//   const referralCode = context.query.referral?.toString();
 
-  return { props: { referralCode: referralCode || undefined } };
-};
+//   return { props: { referralCode: referralCode || undefined } };
+// };
