8b81991be99eebfa789a85567382edb686d3de11
Reinny Almonte <reinny@hookooekoo.co>
2023-07-10 10:22:54 +0200

    feat: parsing referral code

--- reinvest-web-app/src/pages/register.tsx
+++ reinvest-web-app/src/pages/register.tsx
@@ -1,15 +1,10 @@
 import { useRouter } from 'next/router';
 import { RegistrationView } from 'views/register';
-import { RegisterFormFlowProvider } from 'views/register/form-flow';
 
 export default function PageRegister() {
   const router = useRouter();
   const referralQuery = router.query.referral;
   const referralCode = typeof referralQuery === 'string' ? referralQuery : undefined;
 
-  return (
-    <RegisterFormFlowProvider initialStoreFields={{ email: '', referralCode: referralCode, password: '', authenticationCode: '' }}>
-      <RegistrationView />
-    </RegisterFormFlowProvider>
-  );
+  return <RegistrationView referralCode={referralCode} />;
 }
