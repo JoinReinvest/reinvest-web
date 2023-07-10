30baf6134abd79d81c9be4afef9adfbf3b471358
Reinny Almonte <126076121+reinny-hkek@users.noreply.github.com>
2023-07-10 10:37:54 +0200

    test: logging responses (#603)

--- reinvest-web-app/src/views/register/index.tsx
+++ reinvest-web-app/src/views/register/index.tsx
@@ -12,10 +12,14 @@ const InnerRegistrationView = () => {
   const isMounted = useIsMounted();
 
   const {
+    getStoreFields,
     CurrentStepView,
     meta: { isFirstStep },
     moveToPreviousValidStep,
   } = useRegisterFormFlow();
+
+  // eslint-disable-next-line no-console
+  console.info({ storeFields: getStoreFields() });
 
   const shouldDisplayFirstStep = isMounted() && isFirstStep;
   const shouldDisplayRestOfSteps = isMounted() && !isFirstStep;
