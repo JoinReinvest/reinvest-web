39cab5de6e5d5dc37a773612b607258cbc0c5de7
Reinny Almonte <reinny@hookooekoo.co>
2023-07-11 12:50:20 +0200

    feat: displaying the lack of recurring investment

--- reinvest-web-app/src/providers/AccountManagement/flows/recurring-investment/steps/index.tsx
+++ reinvest-web-app/src/providers/AccountManagement/flows/recurring-investment/steps/index.tsx
@@ -1,5 +1,6 @@
 import { StepConfirmation } from './confirmation';
 import { StepCurrentRecurringInvestment } from './current-recurring-investment';
 import { StepLoading } from './loading';
+import { StepNoRecurringInvestment } from './no-recurring-investment';
 
-export const STEPS = [StepLoading, StepCurrentRecurringInvestment, StepConfirmation];
+export const STEPS = [StepLoading, StepCurrentRecurringInvestment, StepConfirmation, StepNoRecurringInvestment];
