3d33a96494d8686dfa4e56309809f8ae5cfd587b
Reinny Almonte <reinny@hookooekoo.co>
2023-07-13 15:40:06 +0200

    style: investment details layout

--- reinvest-web-app/src/views/dashboard/components/InvestmentDetails.tsx
+++ reinvest-web-app/src/views/dashboard/components/InvestmentDetails.tsx
@@ -7,18 +7,16 @@ interface Props {
   onClick?: () => void;
 }
 
-export const InvestmentDetails = ({ earnedMoney, title, onClick }: Props) => {
-  return (
-    <div className="flex justify-between px-24 py-14">
-      <div className="flex items-center gap-8">
-        <Typography variant="paragraph-large">{title}</Typography>
+export const InvestmentDetails = ({ earnedMoney, title, onClick }: Props) => (
+  <div className="flex items-center justify-between gap-8 px-24 py-14">
+    <div className="flex items-center gap-8">
+      <Typography variant="paragraph-large">{title}</Typography>
 
-        <button onClick={onClick}>
-          <IconInfo />
-        </button>
-      </div>
-
-      <Typography variant="paragraph-large">{earnedMoney || 0}</Typography>
+      <button onClick={onClick}>
+        <IconInfo />
+      </button>
     </div>
-  );
-};
+
+    <Typography variant="paragraph-large">{earnedMoney || 0}</Typography>
+  </div>
+);
