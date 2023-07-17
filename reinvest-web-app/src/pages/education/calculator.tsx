0b927c7b33c931f06a5cbbc44b88a376b9b6bd7b
Mateusz Solecki <solecki@hookooekoo.co>
2023-07-17 13:55:14 +0200

    add logged email user to calculator

--- reinvest-web-app/src/pages/education/calculator.tsx
+++ reinvest-web-app/src/pages/education/calculator.tsx
@@ -2,12 +2,15 @@ import { NextPage } from 'next';
 
 import { env } from '../../env';
 import { FullWidthContentLayout } from '../../layouts/FullWidthContentLayout';
+import { useAuth } from '../../providers/AuthProvider';
 
 const Calculator: NextPage = () => {
+  const { user } = useAuth();
+
   return (
     <FullWidthContentLayout>
       <iframe
-        src={`${env.site.url}/calculator-iframe`}
+        src={`${env.site.url}/calculator-iframe?email=${user?.attributes?.email || ''}`}
         className="h-screen w-full"
         title="Calculator page"
       />
