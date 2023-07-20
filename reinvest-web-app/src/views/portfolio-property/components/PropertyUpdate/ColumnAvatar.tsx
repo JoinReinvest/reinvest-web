4d1a3f7e1fd110c03c974d1376e391433fe97e31
adammalej <adammalej1@gmail.com>
2023-07-20 13:59:47 +0200

    fix: use avatar initials

--- reinvest-web-app/src/views/portfolio-property/components/PropertyUpdate/ColumnAvatar.tsx
+++ reinvest-web-app/src/views/portfolio-property/components/PropertyUpdate/ColumnAvatar.tsx
@@ -1,5 +1,4 @@
 import { Avatar } from 'components/Avatar';
-import { useMemo } from 'react';
 import { AccountType, Maybe, PortfolioAuthor } from 'reinvest-app-common/src/types/graphql';
 
 import { DashedLine } from './DashedLine';
@@ -9,18 +8,6 @@ interface Props {
 }
 
 export const ColumnAvatar = ({ author }: Props) => {
-  // TO-DO: Deprecate retrieving the initials on our own when
-  // `author.avatar` returns initials.
-  const initials = useMemo(() => {
-    if (!author?.name) return '';
-
-    const nameParts = author?.name.split(' ');
-    const validNameParts = nameParts.filter(Boolean);
-    const initials = validNameParts.map(part => part.charAt(0).toUpperCase());
-
-    return initials.join('');
-  }, [author?.name]);
-
   return (
     <div className="flex flex-col items-center gap-16">
       <div>
@@ -28,7 +15,7 @@ export const ColumnAvatar = ({ author }: Props) => {
           isSizeFixed
           fixedSize="sm"
           src={author?.avatar?.url ?? ''}
-          alt={initials}
+          alt={author?.avatar?.initials ?? ''}
           accountType={AccountType.Individual}
         />
       </div>
